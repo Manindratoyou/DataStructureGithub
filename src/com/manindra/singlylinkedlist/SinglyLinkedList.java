4331ea7125f230735b82052b07b0c067487bc852
manindra <manindratoyou@gmail.com>
2023-03-27 22:56:28 +0530

    insert node at the end of the singly linked list

--- src/com/manindra/singlylinkedlist/SinglyLinkedList.java
+++ src/com/manindra/singlylinkedlist/SinglyLinkedList.java
@@ -24,9 +24,14 @@ public class SinglyLinkedList {
         sll.head.next=second;
         second.next=third;
         third.next=fourth;*/
-        sll.insertFirst(11);
-        sll.insertFirst(8);
-        sll.insertFirst(1);
+
+        //sll.insertFirst(11);
+        //sll.insertFirst(8);
+        //sll.insertFirst(1);
+
+        sll.insertLast(11);
+        sll.insertLast(8);
+        sll.insertLast(1);
         sll.display();
         System.out.println("length is "+sll.findLength());
     }
@@ -34,6 +39,18 @@ public class SinglyLinkedList {
         ListNode newNode=new ListNode(value);
         newNode.next=head;
         head=newNode;
+    }
+    public void insertLast(int value){
+        ListNode newNode=new ListNode(value);
+        if (head==null){
+            head=newNode;
+            return;
+        }
+        ListNode current=head;
+        while (null!=current.next){
+            current=current.next;
+        }
+        current.next=newNode;
     }
     public void display(){
         ListNode current=head;
