6db2ca465b46afef38c07831aadf6040b276a8fe
manindra <manindratoyou@gmail.com>
2023-04-04 18:51:54 +0530

    array simple sum

--- src/com/manindra/test/Test.java
+++ src/com/manindra/test/Test.java
@@ -1,7 +1,26 @@
 package com.manindra.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
+
+        ArrayList al=new ArrayList<>();
+        al.add(10);
+        al.add(20);
+        System.out.println(simpleArraySum(al));
+    }
+
+    public static int simpleArraySum(List<Integer> ar) {
+
+        int sum=0;
+        for(int i=0;i<ar.size();i++){
+            sum=sum+ar.get(i);
+        }
+        return sum;
+
 
     }
 }
