81f42256b42f537fc9ac1c67e3fcb06b10b9e286
manindra <manindratoyou@gmail.com>
2023-04-13 11:18:58 +0530

    create CircularSinglyLinkedList

--- src/com/manindra/circularlinkedlist/CircularSinglyLinkedList.java
+++ src/com/manindra/circularlinkedlist/CircularSinglyLinkedList.java
@@ -2,6 +2,14 @@ package com.manindra.circularlinkedlist;
 
 public class CircularSinglyLinkedList {
 
+    public static void main(String[] args) {
+
+        CircularSinglyLinkedList csll =new CircularSinglyLinkedList();
+        csll.createCircularLinkedList();
+        csll.display();
+
+    }
+
     private ListNode last;
     private int length;
 
@@ -28,4 +36,29 @@ public class CircularSinglyLinkedList {
 
     //==implementation of circular singly linked list in java==//
     //=========================================================
+
+    public void createCircularLinkedList(){
+        ListNode first=new ListNode(5);
+        ListNode second=new ListNode(6);
+        ListNode third=new ListNode(9);
+        ListNode fourth=new ListNode(15);
+
+        first.next=second;
+        second.next=third;
+        third.next=fourth;
+        fourth.next=first;
+
+        last=fourth;
+    }
+
+    public void display(){
+        if (last==null)
+            return;
+        ListNode first=last.next;
+        while (first!=last) {
+            System.out.print(first.data + " ");
+            first=first.next;
+        }
+        System.out.print(first.data+ " ");
+    }
 }
