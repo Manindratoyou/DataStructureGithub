5265861eeb2eab9b093c8b82530d9be9635d335e
manindra <manindratoyou@gmail.com>
2023-04-14 11:23:51 +0530

    remove first node from csll

--- src/com/manindra/circularlinkedlist/CircularSinglyLinkedList.java
+++ src/com/manindra/circularlinkedlist/CircularSinglyLinkedList.java
@@ -1,15 +1,23 @@
 package com.manindra.circularlinkedlist;
 
+import java.util.NoSuchElementException;
+
 public class CircularSinglyLinkedList {
 
     public static void main(String[] args) {
 
         CircularSinglyLinkedList csll =new CircularSinglyLinkedList();
         //csll.createCircularLinkedList();
-        csll.insertFirst(8);
+       /* csll.insertFirst(8);
         csll.insertFirst(20);
         csll.insertFirst(5);
-        csll.insertFirst(98);
+        csll.insertFirst(98);*/
+        csll.insertLast(8);
+        csll.insertLast(20);
+        csll.insertLast(6);
+        csll.insertLast(98);
+
+        System.out.println(csll.removeFirst().data);
 
         csll.display();
 
@@ -76,5 +84,33 @@ public class CircularSinglyLinkedList {
             temp.next=last.next;
         last.next=temp;
         length++;
+    }
+
+    //insert element at last position
+    public void insertLast(int value){
+        ListNode temp=new ListNode(value);
+        if (last==null) {
+            last = temp;
+            last.next=last;
+        }
+        else {
+            temp.next = last.next;
+            last.next=temp;
+            last=temp;
+        }
+        length++;
+    }
+    //remove first node from csll
+    public ListNode removeFirst(){
+        if (isEmpty())
+            throw new NoSuchElementException();
+        ListNode temp=last.next;
+        if (last.next==last)
+            last=null;
+        else
+            last.next=temp.next;
+        temp.next=null;
+        length--;
+        return temp;
     }
 }
