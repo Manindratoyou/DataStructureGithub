bc6d78fb50009a091faf1a21306d60937605e06d
manindra <manindratoyou@gmail.com>
2023-04-15 23:58:48 +0530

    CamelCaseProblem Hacker Rank solution

---
+++ src/com/manindra/util/CamelCaseProblemHackerRank.java
@@ -0,0 +1,21 @@
+package com.manindra.util;
+
+public class CamelCaseProblemHackerRank {
+
+    public static void main(String[] args) {
+
+        System.out.println(camelcase("IdCard"));
+    }
+    public static int camelcase(String s) {
+        int numberOfWords=1;
+        boolean temp;
+        for(int i=1;i<s.length();i++){
+            temp=Character.isUpperCase(s.charAt(i));
+            if(temp)
+                numberOfWords++;
+        }
+        return numberOfWords;
+
+    }
+
+}
