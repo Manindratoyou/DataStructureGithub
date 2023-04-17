b1596ab38ccf7980be1c23bb97cc3fb5fa1b1285
manindra <manindratoyou@gmail.com>
2023-04-17 21:31:10 +0530

    balanced bracket using stack

---
+++ src/com/manindra/stack/BalancedBrackets.java
@@ -0,0 +1,35 @@
+package com.manindra.stack;
+
+import java.util.Stack;
+
+public class BalancedBrackets {
+
+    public static void main(String[] args) {
+
+        String str="{()}";//"{]"
+        System.out.println(isValid(str));
+    }
+
+    public static boolean isValid(String s){
+
+        Stack<Character> stack=new Stack<>();
+        for (char c:s.toCharArray()){
+            if (c=='('||c=='{'||c=='['){
+                stack.push(c);
+            }
+            else {
+                if (stack.isEmpty())
+                    return false;
+                else{
+                    char top=stack.peek();
+                    if ((c==')' && top=='(') || (c=='}' && top=='{') || (c==')' && top=='(')){
+
+                        stack.pop();
+                    }else
+                        return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
