b0a488cb724b1f08c7e17a33e965763f72ccacd9
manindra <manindratoyou@gmail.com>
2023-04-25 00:06:10 +0530

    Max Priority Queue delete Max

--- src/com/manindra/binaryqueue/MaxPriorityQueue.java
+++ src/com/manindra/binaryqueue/MaxPriorityQueue.java
@@ -18,7 +18,78 @@ public class MaxPriorityQueue {
 
     public static void main(String[] args) {
         MaxPriorityQueue pq=new MaxPriorityQueue(3);
-        System.out.println(pq.size());
-        System.out.println(pq.isEmpty());
+
+        pq.insert(4);pq.insert(5);pq.insert(2);
+        pq.insert(6);pq.insert(1);pq.insert(3);
+
+        pq.printMaxHeap();
+        System.out.println();
+
+        pq.deleteMax();
+        pq.printMaxHeap();
+        System.out.println();
+
+
+        System.out.println("size= "+pq.size());
+        System.out.println("Empty or Not= "+pq.isEmpty());
+    }
+
+    //==================================================================//\
+    public void insert(int x){
+        if (n== heap.length-1)
+            resize(2* heap.length);
+        n++;
+        heap[n]=x;
+        swim(n);
+    }
+    private void swim(int k){
+        while (k>1 && heap[k/2]<heap[k]){
+            int temp=heap[k];
+            heap[k]=heap[k/2];
+            heap[k/2]=temp;
+            k=k/2;
+        }
+    }
+    private void resize(int capacity){
+        Integer[] temp=new Integer[capacity];
+        for (int i=0;i<heap.length;i++){
+            temp[i]=heap[i];
+        }
+        heap=temp;
+    }
+
+    //delete max element in max heap
+    public int deleteMax(){
+        int max=heap[1];
+        swap(1,n);
+        n--;
+        sink(1);
+        heap[n+1]=null;
+        if (n>0 && (n==(heap.length-1)/4))
+            resize(heap.length/2);
+        return max;
+    }
+    private void swap(int a,int b){
+        int temp=heap[a];
+        heap[a]=heap[b];
+        heap[b]=temp;
+    }
+    private void sink(int k){
+        while (2*k<=n){
+            int j=2*k;
+            if (j<n && heap[j]<heap[j+1])
+                j++;
+            if (heap[k]>=heap[j])
+                break;
+            swap(k,j);
+            k=j;
+        }
+    }
+
+    //print
+    public void printMaxHeap(){
+        for (int i=0;i<=n;i++){
+            System.out.print(heap[i] + " ");
+        }
     }
 }
