d0a53d700b22f1a99bbcf921a59717d53c5975f2
manindra <manindratoyou@gmail.com>
2023-04-25 00:08:35 +0530

    Hacker Rank In String

---
+++ src/com/manindra/util/HackerRankInString.java
@@ -0,0 +1,23 @@
+package com.manindra.util;
+
+public class HackerRankInString {
+
+    public static void main(String[] args) {
+        System.out.println(hackerrankInString("hereiamstackerrank"));
+    }
+    public static String hackerrankInString(String s) {
+        String hackerrank = "hackerrank";
+        int index = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == hackerrank.charAt(index)) {
+                index++;
+            }
+            if (index == hackerrank.length()) {
+                return "YES";
+            }
+        }
+        return "NO";
+
+    }
+
+}
