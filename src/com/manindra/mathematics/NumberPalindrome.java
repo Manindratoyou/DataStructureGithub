e85d7e287e962d044708895fe61f2383629313ca
manindra <manindratoyou@gmail.com>
2023-04-27 19:21:54 +0530

    Number Palindrome

---
+++ src/com/manindra/mathematics/NumberPalindrome.java
@@ -0,0 +1,20 @@
+package com.manindra.mathematics;
+
+public class NumberPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(palindrome(363));
+        System.out.println(3%10);
+        System.out.println(3/10);
+    }
+    static boolean palindrome(int number){
+        int rev=0;
+        int temp=number;
+        while (temp!=0){
+            int ld=temp%10;
+            rev=rev*10+ld;
+            temp=temp/10;
+        }
+        return rev==number;
+    }
+}
