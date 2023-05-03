ecbb38c71d4a91b258f06b4c5863a220787d9169
manindra <manindratoyou@gmail.com>
2023-05-03 18:17:29 +0530

    third highest in array

---
+++ src/com/manindra/util/ThirdHighest.java
@@ -0,0 +1,23 @@
+package com.manindra.util;
+
+public class ThirdHighest {
+
+    public static void main(String[] args) {
+
+        int [] arr={7,5,8,25,40,65};
+        int highest=arr[0];
+        int secondHighest=arr[0];
+        int thirdHighest=arr[0];
+
+        for (int i=1;i< arr.length;i++){
+            if (arr[i]>highest)
+                thirdHighest=secondHighest;
+            secondHighest=highest;
+            highest=arr[i];
+        }
+        System.out.println("first "+highest);
+        System.out.println("second "+secondHighest);
+        System.out.println("third "+thirdHighest);
+
+    }
+}
