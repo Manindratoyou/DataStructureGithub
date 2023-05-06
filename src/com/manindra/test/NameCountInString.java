571a29e6e8bdcce9a1dd43a3fd581512272ca702
manindra <manindratoyou@gmail.com>
2023-05-06 21:08:29 +0530

    Name count in a String

---
+++ src/com/manindra/test/NameCountInString.java
@@ -0,0 +1,25 @@
+package com.manindra.test;
+
+public class NameCountInString {
+
+    public static void main(String[] args) {
+        System.out.println(nameCount("manindramanindra"));//manindramaninnndra
+    }
+
+    public static int  nameCount(String s){
+
+        String name="Manindra";
+        int index=0;
+        int count=0;
+
+        for (int i=0;i<s.length();i++){
+            if (s.charAt(i)==name.charAt(index))
+                index++;
+            if (index==name.length()) {
+                count++;
+                index=0;
+            }
+        }
+        return count;
+    }
+}
