3c065928a555f64882fd13ea3a98276fe68434fd
manindra <manindratoyou@gmail.com>
2023-05-18 21:32:05 +0530

    PrintCharacterFrequency

---
+++ src/com/manindra/util/PrintCharacterFrequency.java
@@ -0,0 +1,47 @@
+package com.manindra.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrintCharacterFrequency {
+
+    public static void main(String[] args) {
+
+        String str = "ascbakhbesiakkava";
+        int[] feq = getFrequencyByArray(str);
+        int temp = 97;
+        for (int s : feq) {
+            String s1 = (char) temp++ + "   " + s;
+            System.out.println(s1);
+        }
+        System.out.println();
+        Map<Character, Integer> byHashMap = getFrequencyByHashMap(str);
+        //byHashMap.forEach((key,val)-> System.out.println("char="+key+" "+"count=" +val));
+
+        for (Map.Entry<Character, Integer> entry : byHashMap.entrySet()) {
+            Character k = entry.getKey();
+            Integer v = entry.getValue();
+            System.out.println("Key: " + k + ", Value: " + v);
+        }
+    }
+
+    static int[] getFrequencyByArray(String input) {
+        int[] frequency = new int[26];
+        for (int i = 0; i < input.length(); i++) {
+            char currentCharacter = input.charAt(i);
+            frequency[currentCharacter - 'a']++;
+        }
+        return frequency;
+    }
+
+    static Map<Character, Integer> getFrequencyByHashMap(String input) {
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        for (int i = 0; i < input.length(); i++) {
+            char currentCharacter = input.charAt(i);
+            int freq = frequencyMap.getOrDefault(currentCharacter, 0);
+            frequencyMap.put(currentCharacter, ++freq);
+        }
+        return frequencyMap;
+    }
+
+}
