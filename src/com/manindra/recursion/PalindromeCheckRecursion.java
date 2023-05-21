033ab2dde6fd875c65afd31410f17127b20395f7
manindra <manindratoyou@gmail.com>
2023-05-21 11:07:21 +0530

    PalindromeCheckRecursion

---
+++ src/com/manindra/recursion/PalindromeCheckRecursion.java
@@ -0,0 +1,15 @@
+package com.manindra.recursion;
+
+public class PalindromeCheckRecursion {
+
+    public static void main(String[] args) {
+        String str="abbcbba";
+        System.out.println(isPalindrome(str,0,str.length()-1));
+    }
+
+    static boolean isPalindrome(String str,int start,int end){
+        if (start>=end)
+            return true;
+        return (str.charAt(start)==str.charAt(end) && isPalindrome(str,start+1,end-1));
+    }
+}
