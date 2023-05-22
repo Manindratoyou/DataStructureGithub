bb4580614c88c4a78344dbdce4a25f9ccc3289e2
manindra <manindratoyou@gmail.com>
2023-05-23 00:56:36 +0530

    GenerateSubset recursion

---
+++ src/com/manindra/recursion/GenerateSubset.java
@@ -0,0 +1,22 @@
+package com.manindra.recursion;
+
+public class GenerateSubset {
+
+    public static void main(String[] args) {
+
+        String str = "ABC";
+        printSub(str, "", 0);
+    }
+
+    static void printSub(String str, String curr, int index)
+    {
+        if(index == str.length())
+        {
+            System.out.print(curr+" ");
+            return;
+        }
+
+        printSub(str, curr, index + 1);
+        printSub(str, curr+str.charAt(index), index + 1);
+    }
+}
