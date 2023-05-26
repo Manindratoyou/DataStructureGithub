765e6db8b8f85499b49ed5860ee36fccc64c726a
manindra <manindratoyou@gmail.com>
2023-05-26 06:00:41 +0530

    MoveZero at the end in an array modified code

--- src/com/manindra/array/MoveZeroLast.java
+++ src/com/manindra/array/MoveZeroLast.java
@@ -5,8 +5,35 @@ public class MoveZeroLast {
     public static void main(String[] args) {
 
         int [] arr={8,1,0,2,1,0,3};
-        moveZero(arr, arr.length);
+        //moveZero(arr, arr.length);
+       // moveZeroToEndSolOne(arr, arr.length);
+        moveZeroToEndSolTwo(arr, arr.length);
         printArray(arr);
+    }
+    static void moveZeroToEndSolOne(int [] arr,int n){ //tc O(n2)
+        for (int i=0;i<n;i++){
+            if (arr[i]==0){
+                for (int j=i+1;j<n;j++){
+                    if (arr[j]!=0) {
+                        // swap(arr[i],arr[j]);
+                        int temp = arr[i];
+                        arr[i]=arr[j];
+                        arr[j]=temp;
+                    }
+                }
+            }
+        }
+    }
+    static void moveZeroToEndSolTwo(int [] arr,int n){ //tc O(n) as O(1)
+        int count =0;
+        for (int i=0;i<n;i++){
+            if (arr[i]!=0){
+                int temp = arr[i];
+                arr[i]=arr[count];
+                arr[count]=temp;
+                count++;
+            }
+        }
     }
 
     static void moveZero(int[] arr,int n){
