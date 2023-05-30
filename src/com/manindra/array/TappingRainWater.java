27c45b4e736f463203104938c29b4dc0dc50cd50
manindra <manindratoyou@gmail.com>
2023-05-30 12:25:51 +0530

    Tapping Rain Water problem code modified

--- src/com/manindra/array/TappingRainWater.java
+++ src/com/manindra/array/TappingRainWater.java
@@ -4,7 +4,8 @@ public class TappingRainWater {
 
     public static void main(String[] args) {
         int [] arr={3,0,1,2,5};
-        System.out.println(getWaterSolOne(arr));
+        //System.out.println(getWaterSolOne(arr));
+        System.out.println(getWaterSolTwo(arr));
     }
     static int getWaterSolOne(int [] arr){
         int result=0;
@@ -20,6 +21,24 @@ public class TappingRainWater {
 
             result=result+(Math.min(lMax,rMax)-arr[i]);
         }
+        return result;
+    }
+    static int getWaterSolTwo(int [] arr){
+        int result=0;
+        int [] lMax=new int[arr.length];
+        int [] rMax=new int[arr.length];
+
+        lMax[0]=arr[0];
+        for (int i=1;i< arr.length;i++)
+            lMax[i]=Math.max(arr[i],lMax[i-1]);
+
+        rMax[arr.length-1]=arr[arr.length-1];
+        for (int i= arr.length-2;i>=0;i--)
+            rMax[i]=Math.max(arr[i],rMax[i+1]);
+
+        for(int i = 1; i < arr.length - 1; i++)
+            result = result + (Math.min(lMax[i], rMax[i]) - arr[i]);
+
         return result;
     }
 }
