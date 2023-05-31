bbaec42fca7a59b5282a87617154ad05459083cc
manindra <manindratoyou@gmail.com>
2023-05-31 11:22:03 +0530

    MaxConsecutiveOnes in binary array modified code

--- src/com/manindra/array/MaxConsecutiveOnes.java
+++ src/com/manindra/array/MaxConsecutiveOnes.java
@@ -5,7 +5,8 @@ public class MaxConsecutiveOnes {
     public static void main(String[] args) {
 
         int arr[] = {0, 1, 1, 1, 0, 1, 1};
-        System.out.println(maxConsecutiveOnesSolOne(arr));
+        //System.out.println(maxConsecutiveOnesSolOne(arr));
+        System.out.println(maxConsecutiveOnesSolTwo(arr));
     }
 
     static int maxConsecutiveOnesSolOne(int [] arr){
@@ -20,6 +21,20 @@ public class MaxConsecutiveOnes {
                     break;
             }
             result=Math.max(current,result);
+        }
+        return result;
+    }
+    static int maxConsecutiveOnesSolTwo(int [] arr){
+
+        int result=0,current=0;
+        int n=arr.length;
+        for (int i=0;i<n;i++){
+            if (arr[i]==0)
+                current=0;
+            else {
+                current++;
+                result=Math.max(result,current);
+            }
         }
         return result;
     }
