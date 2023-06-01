2c70b1f52f91150bcfda08761ad3d50f5e59b6f9
manindra <manindratoyou@gmail.com>
2023-06-01 13:22:46 +0530

    MaximumSumCircularSubArray problem in java

--- src/com/manindra/array/MaximumSumCircularSubArray.java
+++ src/com/manindra/array/MaximumSumCircularSubArray.java
@@ -4,9 +4,10 @@ public class MaximumSumCircularSubArray {
 
     public static void main(String[] args) {
         int [] arr={5,-2,3,4};
-        System.out.println(maxCircularSumArray(arr,arr.length));
+        //System.out.println(maxCircularSumArraySolOne(arr,arr.length));
+        System.out.println(maxCircularSumArraySolTwo(arr,arr.length));
     }
-    static int maxCircularSumArray(int [] arr,int n){
+    static int maxCircularSumArraySolOne(int [] arr,int n){
         int result=arr[0];
         for (int i=0;i<n;i++){
             int current_max=arr[i];
@@ -17,6 +18,31 @@ public class MaximumSumCircularSubArray {
                 current_max=Math.max(current_max,current_sum);
             }
             result=Math.max(result,current_max);
+        }
+        return result;
+    }
+    static int maxCircularSumArraySolTwo(int [] arr,int n){
+
+        int max_normal=normalMaxSum(arr,n);
+        if (max_normal<0)
+            return max_normal;
+        int arr_sum=0;
+        for (int i=0;i<n;i++){
+            arr_sum+=arr[i];
+            arr[i]=-arr[i];
+        }
+        int max_circular=arr_sum+normalMaxSum(arr, n);
+        return Math.max(max_normal,max_circular);
+    }
+
+    static int normalMaxSum(int [] arr,int n){
+        int result=arr[0];
+        for (int i=0;i<n;i++){
+            int current=0;
+            for (int j=i;j<n;j++){
+                current=current+arr[j];
+                result=Math.max(result,current);
+            }
         }
         return result;
     }
