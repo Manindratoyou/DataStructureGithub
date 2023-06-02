322a493885f4d4a25b892fe2e0c2ed10547eab7a
manindra <manindratoyou@gmail.com>
2023-06-02 13:12:26 +0530

    MajorityElement in array

--- src/com/manindra/array/MajorityElement.java
+++ src/com/manindra/array/MajorityElement.java
@@ -5,6 +5,7 @@ public class MajorityElement {
     public static void main(String[] args) {
         int [] arr={8,3,4,8,8};
         System.out.println(findMajority(arr, arr.length));//majority element more than n/2 times
+        System.out.println(findMajoritySolTwo(arr, arr.length));
     }
     static int findMajority(int [] arr,int n){
         for (int i=0;i<n;i++){
@@ -17,5 +18,31 @@ public class MajorityElement {
             return i;
         }
         return -1;
+    }
+    static int findMajoritySolTwo(int [] arr,int n){
+        int res = 0, count = 1;
+
+        for(int i = 1; i < n; i++)
+        {
+            if(arr[res] == arr[i])
+                count++;
+            else
+                count --;
+
+            if(count == 0)
+            {
+                res = i; count = 1;
+            }
+        }
+
+        count = 0;
+        for(int i = 0; i < n; i++)
+            if(arr[res] == arr[i])
+                count++;
+
+        if(count <= n /2)
+            res = -1;
+
+        return res;
     }
 }
