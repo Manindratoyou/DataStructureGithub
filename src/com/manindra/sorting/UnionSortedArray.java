60598e9de429e36cb9edf77c87569ef87e436873
manindra <manindratoyou@gmail.com>
2023-06-12 22:49:03 +0530

    unionTwoSortedArray solution two

--- src/com/manindra/sorting/UnionSortedArray.java
+++ src/com/manindra/sorting/UnionSortedArray.java
@@ -7,7 +7,8 @@ public class UnionSortedArray {
     public static void main(String[] args) {
       int [] arr1={3,5,8};
       int [] arr2={2,8,9,10,15};
-        unionTwoSortedArraySolOne(arr1,arr2, arr1.length, arr2.length);
+       // unionTwoSortedArraySolOne(arr1,arr2, arr1.length, arr2.length);
+        unionTwoSortedArraySolTwo(arr1,arr2, arr1.length, arr2.length);
     }
 
     static void unionTwoSortedArraySolOne(int [] arr1,int[] arr2,int m,int n){
@@ -21,6 +22,44 @@ public class UnionSortedArray {
             if (i==0 || arr3[i]!=arr3[i-1])
                 System.out.print(arr3[i]+" ");
         }
+    }
+    static void unionTwoSortedArraySolTwo(int [] arr1,int[] arr2,int m,int n){
+        int i=0,j=0;
+        while (i<m && j<n){
+            if (i>0 && arr1[i]==arr1[i-1]){
+                i++;
+                continue;
+            }
+            if (j>0 && arr2[j]==arr2[j-1]){
+                j++;
+                continue;
+            }
+            if (arr1[i]<arr2[j]){
+                System.out.print(arr1[i] +" ");
+                i++;
+            } else if (arr1[i]>arr2[j]) {
+                System.out.print(arr2[j]+" ");
+                j++;
+            }
+            else {
+                System.out.print(arr1[i] +" ");
+                i++;
+                j++;
+            }
+        }//while loop ends
+        while (i<m){
+            if (i>0 && arr1[i]!=arr1[i-1]){
+                System.out.print(arr1[i]+" ");
+                i++;
+            }
+        }
+        while (j<n){
+            if (j>0 && arr2[j]!=arr2[j-1]){
+                System.out.print(arr2[j] +" ");
+                j++;
+            }
+        }
+
     }
 
 }
