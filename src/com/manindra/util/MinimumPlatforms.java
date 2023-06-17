ab7f3489952a007d925632acb374feed0423abe1
manindra <manindratoyou@gmail.com>
2023-06-18 00:45:46 +0530

    MinimumPlatforms updated code

--- src/com/manindra/util/MinimumPlatforms.java
+++ src/com/manindra/util/MinimumPlatforms.java
@@ -3,6 +3,15 @@ package com.manindra.util;
 import java.util.Arrays;
 
 public class MinimumPlatforms {
+
+    public static void main(String[] args) {
+        int[] arr = {900, 940, 950, 1100, 1500, 1800};
+        int[] dep = {910, 1200, 1120, 1130, 1900, 2000};
+
+        //int minPlatforms = findMinPlatforms(arr, dep);
+        int minPlatforms = meetingMaxGuest(arr, dep);
+        System.out.println("Minimum number of platforms required: " + minPlatforms);
+    }
     public static int findMinPlatforms(int[] arr, int[] dep) {
         Arrays.sort(arr);
         Arrays.sort(dep);
@@ -25,12 +34,22 @@ public class MinimumPlatforms {
 
         return maxPlatforms;
     }
+    static int meetingMaxGuest(int [] arival,int [] departure){
+        Arrays.sort(arival);
+        Arrays.sort(departure);
 
-    public static void main(String[] args) {
-        int[] arr = {900, 940, 950, 1100, 1500, 1800};
-        int[] dep = {910, 1200, 1120, 1130, 1900, 2000};
-
-        int minPlatforms = findMinPlatforms(arr, dep);
-        System.out.println("Minimum number of platforms required: " + minPlatforms);
+        int i=1,j=0,maxPlatforms=1,platformsNeeded=1;
+        while (i< arival.length && j< departure.length){
+            if (arival[i]<departure[j]){
+                platformsNeeded++;
+                i++;
+            }else {
+                platformsNeeded--;
+                j++;
+            }
+            maxPlatforms=Math.max(platformsNeeded,maxPlatforms);
+        }
+        return maxPlatforms;
     }
+
 }
