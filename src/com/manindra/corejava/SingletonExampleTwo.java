e52eefc589cc236f62f1d60d3808a1d93cfafb34
manindra <manindratoyou@gmail.com>
2023-06-19 22:51:02 +0530

    Singleton class in Java

---
+++ src/com/manindra/corejava/SingletonExampleTwo.java
@@ -0,0 +1,35 @@
+package com.manindra.corejava;
+
+class SingletonEagar{
+    private static SingletonEagar instance=new SingletonEagar();
+
+    private SingletonEagar(){}
+
+    public static SingletonEagar getInstance(){
+        return instance;
+    }
+}
+class SingletonLazy{
+    private static SingletonLazy instance;
+
+    private SingletonLazy(){}
+
+    public static SingletonLazy getInstance(){
+        if (instance==null)
+            instance=new SingletonLazy();
+        return instance;
+    }
+}
+public class SingletonExampleTwo {
+
+    public static void main(String[] args) {
+
+        SingletonEagar instance1=SingletonEagar.getInstance();
+        System.out.println(instance1+" : Singleton Eager initialization");
+
+        SingletonEagar instance2=SingletonEagar.getInstance();
+        System.out.println(instance2+" : Singleton Lazy initialization");
+
+    }
+
+}
