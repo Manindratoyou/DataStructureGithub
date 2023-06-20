907f9871cf59edc4943642d7693ef6d6b6cc323b
manindra <manindratoyou@gmail.com>
2023-06-21 00:21:24 +0530

    Radix Sort in java

---
+++ src/com/manindra/sorting/RadixSort.java
@@ -0,0 +1,39 @@
+package com.manindra.sorting;
+
+import java.util.Arrays;
+
+public class RadixSort {
+
+    public static void main(String[] args) {
+        int [] arr={319,212,6,8,100,50};
+        radixSort(arr,arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void radixSort(int [] arr, int n){
+        int mx=arr[0];
+        for (int i=1;i<n;i++){
+         if (arr[i]>mx)
+             mx=arr[i];
+        }
+        for (int exp=1;mx/exp>0;exp*=10)
+            countingSortSolTwo(arr, n, exp);
+    }
+
+    static void countingSortSolTwo(int [] arr, int n, int exp){
+
+        int [] output=new int[n];
+        int [] count=new int[10];
+        for (int i=0;i<n;i++)
+            count[(arr[i]/exp)%10]++;
+
+        for (int i=1;i<10;i++)
+            count[i]=count[i-1]+count[i];
+
+        for (int i=n-1;i>=0;i--){
+            output[count[(arr[i]/exp)%10]-1]=arr[i];
+            count[(arr[i])/exp % 10]--;
+        }
+        for (int i=0;i<n;i++)
+            arr[i]=output[i];
+    }
+}
