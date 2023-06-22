bac6d3223a845628858faf2df9183b8ffae20ddd
manindra <manindratoyou@gmail.com>
2023-06-23 00:23:27 +0530

    PatternSearching in java Naive improved code

--- src/com/manindra/string/PatternSearchingNaive.java
+++ src/com/manindra/string/PatternSearchingNaive.java
@@ -5,6 +5,7 @@ public class PatternSearchingNaive {
     public static void main(String[] args) {
         String text="ABABABCD", Pattern="ABAB";
         patternSearchingNaive(text,Pattern);
+        //patternSearchingNaiveImprove(text,Pattern);
     }
 
     static void patternSearchingNaive(String text,String pattern){
@@ -17,6 +18,22 @@ public class PatternSearchingNaive {
                     break;
             if (j==m)
                 System.out.print(i+" ");
+        }
+    }
+    static void patternSearchingNaiveImprove(String text,String pattern){
+        int m=pattern.length();
+        int n=text.length();
+        for (int i=0;i<=(n-m); ){
+            int j;
+            for (j=0;j<m-1;j++)
+                if (pattern.charAt(j)!=text.charAt(i+j))
+                    break;
+            if (j==m)
+                System.out.print(i+" ");
+            if (j==0)
+                i++;
+            else
+                i=i+j;
         }
     }
 }
