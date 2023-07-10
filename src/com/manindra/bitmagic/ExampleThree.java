f7095500bd5f95b34b74c68a93e43399a55f9e37
manindra <manindratoyou@gmail.com>
2023-07-10 22:57:59 +0530

    bitmagic example three  in java

---
+++ src/com/manindra/bitmagic/ExampleThree.java
@@ -0,0 +1,15 @@
+package com.manindra.bitmagic;
+
+public class ExampleThree {
+
+    public static void main(String[] args) {
+
+        int x=33;
+        System.out.println(x>>1);//signed right shift
+        System.out.println(x>>4);
+        int x1=-2;
+        System.out.println(x1>>>1);//unsigned right shift
+        System.out.println(x1>>>4);
+
+    }
+}
