c53f50fb583f4f0a0f678fb7ae62ac469448f2e5
manindra <manindratoyou@gmail.com>
2023-07-10 22:57:34 +0530

    bitmagic example two  in java

---
+++ src/com/manindra/bitmagic/ExampleTwo.java
@@ -0,0 +1,15 @@
+package com.manindra.bitmagic;
+
+public class ExampleTwo {
+
+    public static void main(String[] args) {
+        int x=1;
+        System.out.println(~1); //bitwise not
+        int x1=3;
+        System.out.println(x<<1);//left shift
+        System.out.println(x<<2);
+        int x2=-1;
+        System.out.println(x<<1);
+        System.out.println(x<<2);
+    }
+}
