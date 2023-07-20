36b211a647263cf8abc67c2831eed5f1e82dbb03
manindra <manindratoyou@gmail.com>
2023-07-20 22:41:01 +0530

    FrequencyOfArrayElement solution two

--- src/com/manindra/hashing/FrequencyOfArrayElement.java
+++ src/com/manindra/hashing/FrequencyOfArrayElement.java
@@ -1,10 +1,13 @@
 package com.manindra.hashing;
 
+import java.util.HashMap;
+
 public class FrequencyOfArrayElement {
 
     public static void main(String[] args) {
         int [] arr={10,20,20,30,10};
-        printFrequency(arr);
+        //printFrequency(arr);
+        printFrequencySolTwo(arr);
 
     }
 
@@ -25,5 +28,12 @@ public class FrequencyOfArrayElement {
                     frequency++;
             System.out.println(arr[i]+" "+frequency);
         }
+    }
+    static void printFrequencySolTwo(int [] arr){
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
+        for (int x :arr){
+            hashMap.put(x,hashMap.getOrDefault(x,0)+1);
+        }
+        hashMap.forEach((key,value)-> System.out.println(key+" "+value));
     }
 }
