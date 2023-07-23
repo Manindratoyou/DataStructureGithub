403287f95ffd11f36e5bf806b1d0d24510c5d867
manindra <manindratoyou@gmail.com>
2023-07-23 20:33:02 +0530

    UnionOfTwoUnSortedArray in array different solution

--- src/com/manindra/hashing/UnionOfTwoUnSortedArray.java
+++ src/com/manindra/hashing/UnionOfTwoUnSortedArray.java
@@ -9,8 +9,13 @@ public class UnionOfTwoUnSortedArray {
     public static void main(String[] args) {
         int [] arr1={10,30,10};
         int [] arr2={5,10,5};
+
+        Integer [] arr3={10,30,10};
+        Integer [] arr4={5,10,5};
+
         System.out.println(unionOfTwoUnSortedArraySolOne(arr1,arr2));
         System.out.println(unionCountDistinct(arr1,arr2));
+        System.out.println(unionCountDistinctSolThree(arr3,arr4));
     }
 
     static int unionOfTwoUnSortedArraySolOne(int [] arr1,int [] arr2){ // naive solution
@@ -41,5 +46,11 @@ public class UnionOfTwoUnSortedArray {
         for (int x:arr2)
             set.add(x);
         return set.size();
+    }
+
+    static int unionCountDistinctSolThree(Integer [] arr1,Integer [] arr2){
+       Set<Integer> set= new HashSet<>(Arrays.asList(arr1));
+       set.addAll(Arrays.asList(arr2));
+       return set.size();
     }
 }
