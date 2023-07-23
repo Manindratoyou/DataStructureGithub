41e9fb13086f1f7fb52496e042bc58ce28d8dd3b
manindra <manindratoyou@gmail.com>
2023-07-23 09:29:49 +0530

    IntersectionOfTwoUnsortedArray sol two

--- src/com/manindra/hashing/IntersectionOfTwoUnsortedArray.java
+++ src/com/manindra/hashing/IntersectionOfTwoUnsortedArray.java
@@ -1,11 +1,14 @@
 package com.manindra.hashing;
 
+import java.util.HashSet;
+
 public class IntersectionOfTwoUnsortedArray {
 
     public static void main(String[] args) {
         int [] arr1={10,15,20,25,30,50};
         int [] arr2={30,5,15,80};
         intersectionOfTwoUnsortedArray(arr1,arr2,arr1.length,arr2.length);
+        intersectionOfTwoUnsortedArraySolTwo(arr1,arr2);
 
     }
 
@@ -19,6 +22,16 @@ public class IntersectionOfTwoUnsortedArray {
                 }
             }
             if (flag==true)
+                System.out.print(arr1[i]+" ");
+        }
+    }
+    static void intersectionOfTwoUnsortedArraySolTwo(int [] arr1,int[] arr2){
+        HashSet<Integer> hashSet=new HashSet<>();
+        for (int i=0;i< arr2.length;i++){
+            hashSet.add(arr2[i]);
+        }
+        for (int i=0;i< arr1.length;i++){
+            if (hashSet.contains(arr1[i]))
                 System.out.print(arr1[i]+" ");
         }
     }
