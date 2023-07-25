5647c1592d2663c21ff2b0f2dace5ebc24194418
manindra <manindratoyou@gmail.com>
2023-07-26 00:15:43 +0530

    SubArrayWithZeroSum sol two

--- src/com/manindra/hashing/SubArrayWithZeroSum.java
+++ src/com/manindra/hashing/SubArrayWithZeroSum.java
@@ -1,10 +1,14 @@
 package com.manindra.hashing;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SubArrayWithZeroSum {
 
     public static void main(String[] args) {
         int [] arr={1,4,13,-3,-10,5};//13,-3,-10 sum is zero
-        System.out.println(isZeroSumSolOne(arr));
+        //System.out.println(isZeroSumSolOne(arr));
+        System.out.println(isZeroSumSolTwo(arr));
     }
 
     static boolean isZeroSumSolOne(int [] arr){ //O(n2)
@@ -15,6 +19,20 @@ public class SubArrayWithZeroSum {
                 if (current_sum==0)
                     return true;
             }
+        }
+        return false;
+    }
+    static boolean isZeroSumSolTwo(int [] arr){ //O(n)
+
+        Set<Integer> set=new HashSet<>();
+        int pre_sum=0;
+        for (int i=0;i< arr.length;i++){
+            pre_sum+=arr[i];
+            if (set.contains(pre_sum))
+                return true;
+            if (pre_sum==0) //[-3,2,1]
+                return true;
+            set.add(pre_sum);
         }
         return false;
     }
