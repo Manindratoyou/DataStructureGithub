f9a1f3b46872a049cd651dc5180bec02e00ecfa2
manindra <manindratoyou@gmail.com>
2023-07-29 00:30:11 +0530

    LongestSubArrayWithZerosAndOnes solution two

--- src/com/manindra/hashing/LongestSubArrayWithZerosAndOnes.java
+++ src/com/manindra/hashing/LongestSubArrayWithZerosAndOnes.java
@@ -1,11 +1,15 @@
 package com.manindra.hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubArrayWithZerosAndOnes {
 
     public static void main(String[] args) {
         int [] arr={1,0,1,1,1,0,0}; //op is 6 =>0,1,1,1,0,0
         int [] arr1={1,1,1,1}; //op 0
-        System.out.println(longestSubArrayWithZerosAndOnes(arr));
+        //System.out.println(longestSubArrayWithZerosAndOnes(arr));
+        System.out.println(longestSubArrayWithZerosAndOnesSolTwo(arr));
     }
     static int longestSubArrayWithZerosAndOnes(int [] arr){ //tc 0(n2) as 0(1)
         int result=0;
@@ -21,5 +25,25 @@ public class LongestSubArrayWithZerosAndOnes {
             }
         }
         return result;
+    }
+    static int longestSubArrayWithZerosAndOnesSolTwo(int [] arr){
+        Map<Integer,Integer> map=new HashMap<>();
+        int sum=0,max_len=0;
+        for (int i=0;i<arr.length;i++){
+            arr[i]=(arr[i]==0 ) ? -1 : 1 ;
+        }
+        for (int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if (sum==0)
+                max_len=i+1;
+            if (map.containsKey(sum+arr.length)==true){
+                if (max_len<i-map.get(sum+arr.length))
+                    max_len=i-map.get(sum+arr.length);
+            }else {
+                map.put(sum+arr.length ,i);
+            }
+
+        }
+        return max_len;
     }
 }
