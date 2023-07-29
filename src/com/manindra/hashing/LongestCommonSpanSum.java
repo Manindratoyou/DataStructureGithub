ad0e9e77d6475a48d3d187bf4fb3d3ca52fc2dc4
manindra <manindratoyou@gmail.com>
2023-07-29 09:44:14 +0530

    LongestCommonSpanSum sol two

--- src/com/manindra/hashing/LongestCommonSpanSum.java
+++ src/com/manindra/hashing/LongestCommonSpanSum.java
@@ -1,12 +1,16 @@
 package com.manindra.hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestCommonSpanSum {
 
     public static void main(String[] args) {
 
         int [] arr1={0,1,0,0,0,0};//1,0,0,0
         int [] arr2={1,0,1,0,0,1}; //0,1,0,0  op is 4
-        System.out.println(maxCommonSum(arr1,arr2));
+        //System.out.println(maxCommonSum(arr1,arr2));
+        System.out.println(maxCommonSumSolTwo(arr1,arr2));
 
     }
 
@@ -22,5 +26,25 @@ public class LongestCommonSpanSum {
             }
         }
         return result;
+    }
+    static int maxCommonSumSolTwo(int [] arr1,int [] arr2){ //tc 0(n) as 0(n)
+
+        //find difference between the two
+        int [] temp=new int[arr1.length];
+        for (int i=0;i< arr1.length;i++)
+            temp[i]=arr1[i]-arr2[i];
+        Map<Integer,Integer> map=new HashMap<>();
+        int sum=0,max_len=0;
+        for (int i=0;i< arr1.length;i++){
+            sum+=temp[i];
+            if (sum==0)
+                max_len=i+1;
+            //if the sum is seen before than update max_len if required
+            if (map.containsKey(sum))
+                max_len=Math.max(max_len,i-map.get(sum));
+            else
+                map.put(sum,i);
+        }
+        return max_len;
     }
 }
