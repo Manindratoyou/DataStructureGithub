d3409e1f7d70ae96e2c0ff34f7750b5edf82322c
manindra <manindratoyou@gmail.com>
2023-07-31 23:56:56 +0530

    LongestConsecutiveSubsequence solution two

--- src/com/manindra/hashing/LongestConsecutiveSubsequence.java
+++ src/com/manindra/hashing/LongestConsecutiveSubsequence.java
@@ -1,11 +1,15 @@
 package com.manindra.hashing;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestConsecutiveSubsequence {
 
     public static void main(String[] args) {
         int [] arr={1,9,3,4,2,10,20}; //op is 4 [1,2,3,4] not 2 [9,10]
+        System.out.println(longestConsecutiveSubsequence(arr));
+        System.out.println(longestConsecutiveSubsequenceSolTwo(arr));
     }
 
     static int longestConsecutiveSubsequence(int [] arr){ //tc 0(nlogn)
@@ -20,5 +24,20 @@ public class LongestConsecutiveSubsequence {
             }
         }
         return Math.max(result,current);
+    }
+    static int longestConsecutiveSubsequenceSolTwo(int [] arr){ //tc 0(n) as 0(n)
+        Set<Integer> set=new HashSet<>();
+        for (int x:arr)
+            set.add(x);
+        int result=1;
+        for (int y :set){
+            if (set.contains(y-1)==false){
+                int current=1;
+                while (set.contains(y+current))
+                    current++;
+                result=Math.max(result,current);
+            }
+        }
+        return result;
     }
 }
