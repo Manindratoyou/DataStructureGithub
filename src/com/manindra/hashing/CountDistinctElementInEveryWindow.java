ab67e1db8b055900528b37c8c53d24f991188fe4
manindra <manindratoyou@gmail.com>
2023-08-03 23:53:08 +0530

    CountDistinctElementInEveryWindow solution two

--- src/com/manindra/hashing/CountDistinctElementInEveryWindow.java
+++ src/com/manindra/hashing/CountDistinctElementInEveryWindow.java
@@ -1,11 +1,14 @@
 package com.manindra.hashing;
 
+import java.util.HashMap;
+
 public class CountDistinctElementInEveryWindow {
 
     public static void main(String[] args) {
         int [] arr={10,20,20,10,30,40,10};
         int k=4;
         printDistinct(arr, arr.length, k);
+        printDistinctSolTwo(arr, arr.length, k);
     }
 
     static void printDistinct(int [] arr,int n,int k){
@@ -23,5 +26,21 @@ public class CountDistinctElementInEveryWindow {
             }
             System.out.println(count);
         }
+    }
+    static void printDistinctSolTwo(int [] arr,int n,int k){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for (int i=0;i<k;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        System.out.println(map.size()+" ");
+        for (int i = k; i < n; i++) {
+            map.put(arr[i - k], map.get(arr[i - k]) - 1);
+            if (map.get(arr[i - k]) == 0) {
+                map.remove(arr[i - k]);
+            }
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            System.out.print(map.size() + " ");
+        }
+        System.out.println();
     }
 }
