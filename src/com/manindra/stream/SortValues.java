9c21d4e2f99866807a36d553a3e9fd9a325545ce
manindra <manindratoyou@gmail.com>
2023-08-07 23:06:01 +0530

    SortValues ascending order

---
+++ src/com/manindra/stream/SortValues.java
@@ -0,0 +1,13 @@
+package com.manindra.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SortValues {
+
+    public static void main(String[] args) {
+
+        List<Integer> list=Arrays.asList(10,16,8,56,5,20);
+        list.stream().sorted().forEach(System.out::println);
+    }
+}
