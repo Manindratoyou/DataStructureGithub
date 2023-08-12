359114668de09adb420fba3c494cbfeaa6097a22
manindra <manindratoyou@gmail.com>
2023-08-12 23:06:35 +0530

    TransposeMatrix solution two

--- src/com/manindra/matrix/TransposeMatrix.java
+++ src/com/manindra/matrix/TransposeMatrix.java
@@ -8,7 +8,8 @@ public class TransposeMatrix {
     public static void main(String[] args) {
 
         int [] [] mat={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        transposeSolOne(mat);
+        //transposeSolOne(mat);
+        transposeSolTwo(mat);
         Arrays.stream(mat).forEach(row->{Arrays.stream(row).forEach(element-> System.out.print(element+" "));
             System.out.println();});
     }
@@ -26,5 +27,16 @@ public class TransposeMatrix {
             for (int j=0;j<n;j++)
                 mat[i][j]=tempMat[i][j];
         }
+    }
+    static void transposeSolTwo(int [] [] mat){
+        for (int i=0;i<n;i++){
+            for (int j=i+1;j<n;j++)
+                swap(mat,i,j);
+        }
+    }
+    static void swap(int [] [] mat,int i,int j){
+        int temp=mat[i][j];
+        mat[i][j]=mat[j][i];
+        mat[j][i]=temp;
     }
 }
