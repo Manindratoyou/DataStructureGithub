f5542ecc3bd7ff94ded08db46acd14123d38eb94
manindra <manindratoyou@gmail.com>
2023-08-12 22:45:14 +0530

    TransposeMatrix solution one

---
+++ src/com/manindra/matrix/TransposeMatrix.java
@@ -0,0 +1,30 @@
+package com.manindra.matrix;
+
+import java.util.Arrays;
+
+public class TransposeMatrix {
+
+    static int n=4;
+    public static void main(String[] args) {
+
+        int [] [] mat={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        transposeSolOne(mat);
+        Arrays.stream(mat).forEach(row->{Arrays.stream(row).forEach(element-> System.out.print(element+" "));
+            System.out.println();});
+    }
+
+    static void transposeSolOne(int [] [] mat){
+        int [] [] tempMat=new int[n][n];
+        //copy mat[][] to temp[][]
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                tempMat[i][j]=mat[j][i];
+            }
+        }
+        //copy temp[][] to mat
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++)
+                mat[i][j]=tempMat[i][j];
+        }
+    }
+}
