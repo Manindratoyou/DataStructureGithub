94ffde0c5ff7607ec2b1ae92e585543de6b684ae
manindra <manindratoyou@gmail.com>
2023-08-13 00:16:50 +0530

    CountDuplicateElement stream

---
+++ src/com/manindra/stream/CountDuplicateElement.java
@@ -0,0 +1,19 @@
+package com.manindra.stream;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CountDuplicateElement {
+
+    public static void main(String[] args) {
+
+        List<String> list= Arrays.asList("AA","BB","CC","DD","AA");
+        Map<String,Long> map=list.stream().filter(x-> Collections.frequency(list,x)>1).collect
+                (Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        System.out.println(map);
+    }
+}
