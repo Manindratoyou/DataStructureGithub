9aa59482e52c9b6910788104b30fdfa98adbb87c
manindra <manindratoyou@gmail.com>
2023-08-13 00:16:20 +0530

    CountEachCharacter stream

---
+++ src/com/manindra/stream/CountEachCharactor.java
@@ -0,0 +1,19 @@
+package com.manindra.stream;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CountEachCharactor {
+
+    public static void main(String[] args) {
+
+        String s = "string data to count each character";
+        Map<String, Long> charCount = Arrays.stream(s.split(""))
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(str -> str, LinkedHashMap::new, Collectors.counting()));
+
+        charCount.forEach((character, count) -> System.out.println(character + ": " + count));
+    }
+}
