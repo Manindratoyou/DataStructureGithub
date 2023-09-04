1d422848ada7b06705f6a59c6aa0fd56ecdef231
manindra <manindratoyou@gmail.com>
2023-09-05 00:06:38 +0530

    leetcode Solution5

---
+++ src/com/manindra/leetcode/Solution5.java
@@ -0,0 +1,75 @@
+package com.manindra.leetcode;
+
+//longest palindromic Substring
+public class Solution5 {
+
+    public static void main(String[] args) {
+
+        Solution5 solution = new Solution5();
+        //System.out.println(solution.longestPalindrome("babad"));
+        System.out.println(solution.longestPalindromeSolTwo("babad"));
+    }
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    public String longestPalindromeSolTwo(String s) {
+        if (s == null || s.length() < 1) return "";
+
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+
+        int start = 0, end = 0;//variables to keep track of the longest palindrome substring
+
+        //all individual character are palindromic
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        //check for palindromes of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                start = i;
+                end = i + 1;
+            }
+        }
+
+        //check for the palindromes of length greater than 2
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    if (j - i > end - start) {
+                        start = i;
+                        end = j;
+                    }
+                }
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+}
