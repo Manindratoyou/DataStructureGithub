78a9b1390ee88192ffae1574969973a30607f0aa
manindra <manindratoyou@gmail.com>
2023-09-06 21:37:02 +0530

    LeftStarTriangle pattern

---
+++ src/com/manindra/pattern/LeftStarTriangle.java
@@ -0,0 +1,17 @@
+package com.manindra.pattern;
+
+public class LeftStarTriangle {
+
+    public static void main(String[] args) {
+        int row = 10;
+        for (int i = 1; i <= row ; i++) {
+            for (int j = 1; j <=  row  - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
