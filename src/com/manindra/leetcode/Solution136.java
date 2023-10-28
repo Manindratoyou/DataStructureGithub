cd03b288b5f3453bac6a2abc54033c0a3dc444ff
manindra <manindratoyou@gmail.com>
2023-10-28 11:27:34 +0530

    leet code solution 136

---
+++ src/com/manindra/leetcode/Solution136.java
@@ -0,0 +1,32 @@
+package com.manindra.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution136 {
+
+    public static void main(String[] args) {
+
+        int [] arr={2,2,1};
+        System.out.println(singleNumber(arr));
+        System.out.println(singleNumberSolutionTwo(arr));
+    }
+
+    static int singleNumber(int [] arr){
+
+        int result=0;
+        for (int num : arr)
+            result^=num;
+        return result;
+    }
+    static int singleNumberSolutionTwo(int [] arr){
+        Set<Integer> set=new HashSet<>();
+        for (int num : arr){
+            if (set.contains(num))
+                set.remove(num);
+            else
+                set.add(num);
+        }
+        return set.iterator().next();
+    }
+}
