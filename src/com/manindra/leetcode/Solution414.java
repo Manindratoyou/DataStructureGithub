12d433d3baea8d1b66be9845326880d763fba258
manindra <manindratoyou@gmail.com>
2023-11-15 07:54:41 +0530

    leet code Solution414 third max

---
+++ src/com/manindra/leetcode/Solution414.java
@@ -0,0 +1,57 @@
+package com.manindra.leetcode;
+
+import java.util.TreeSet;
+
+public class Solution414 { //third max
+
+    public static void main(String[] args) {
+        int [] num1={3,2,1,6,9,32};
+        int [] num2={1,2};
+        int [] num3={2,2,3,1};
+        //System.out.println(thirdMax(num1));
+        //System.out.println(thirdMax(num2));
+
+        System.out.println(thirdMaxSolutionTwo(num1));
+        System.out.println(thirdMaxSolutionTwo(num3));
+    }
+
+    static int thirdMax(int [] nums){
+
+        TreeSet<Integer> set=new TreeSet<>((a,b)->b-a);
+        for (int num : nums)
+            set.add(num);
+        if (set.size()<3)
+            return set.first();
+        set.pollFirst();
+        set.pollFirst();
+
+        return set.first();
+    }
+
+    static int thirdMaxSolutionTwo(int [] nums){
+
+        Integer firstMax=null;
+        Integer secondMax=null;
+        Integer thirdMax=null;
+
+        for (Integer num : nums){
+            //1. skip duplicates
+            if (num.equals(firstMax) ||num.equals(secondMax) || num.equals(thirdMax))
+                continue;
+
+            //2. update top three maximum number
+            if (firstMax==null || num>firstMax){
+                thirdMax=secondMax;
+                secondMax=firstMax;
+                firstMax=num;
+            } else if (secondMax==null || num>secondMax) {
+                thirdMax=secondMax;
+                secondMax=num;
+            } else if (thirdMax==null || num>thirdMax) {
+                thirdMax=num;
+            }
+        }
+        //3. return result
+        return thirdMax!=null ? thirdMax :firstMax;
+    }
+}
