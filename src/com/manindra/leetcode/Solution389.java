3cd83870fa701f96310bfb6908c217a12d3b11e0
manindra <manindratoyou@gmail.com>
2023-11-17 00:12:26 +0530

    leet code Solution389 Find the Difference

---
+++ src/com/manindra/leetcode/Solution389.java
@@ -0,0 +1,53 @@
+package com.manindra.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution389 { //Find the Difference
+
+    public static void main(String[] args) {
+
+        System.out.println(findTheDifference("abcd", "abcde")); // Output: e
+        System.out.println(findTheDifferenceSolutionTwo("abcd", "abcde")); // Output: e
+    }
+
+    static char findTheDifference(String s,String t){
+        char [] sArray=s.toCharArray();
+        char [] tArray=t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        for (int i=0;i< sArray.length;i++){
+            if (sArray[i] !=tArray[i])
+                return tArray[i];
+        }
+        return tArray[tArray.length-1];
+    }
+    static char findTheDifferenceSolutionTwo(String s,String t){
+
+        Map<Character,Integer> map=new HashMap<>();
+
+        for (char c: s.toCharArray())
+            map.put(c,map.getOrDefault(c,0)+1);
+        for (char c: t.toCharArray()){
+            int count=map.getOrDefault(c,0);
+            if (count==0)
+                return c;
+            map.put(c,count-1);
+        }
+        return 0; // This should not be reached if input is valid
+    }
+
+    static char findTheDifferenceSolutionThree(String s, String t) {
+        char result = 0;
+        for (char c : s.toCharArray()) {
+            result ^= c;
+        }
+        for (char c : t.toCharArray()) {
+            result ^= c;
+        }
+        return result;
+    }
+}
