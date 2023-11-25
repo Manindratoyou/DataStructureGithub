7af64a6f6e80a5b95a57dcd6f8fdea15e3d088c0
manindra <manindratoyou@gmail.com>
2023-11-25 23:58:29 +0530

    updated code

--- src/com/manindra/util/ThirdHighest.java
+++ src/com/manindra/util/ThirdHighest.java
@@ -5,20 +5,24 @@ public class ThirdHighest {
     public static void main(String[] args) {
 
         int [] arr={7,5,8,25,40,65};
-        int highest=arr[0];
-        int secondHighest=arr[0];
-        int thirdHighest=arr[0];
-
-        for (int i=1;i< arr.length;i++){
-            if (arr[i]>highest) {
+        int firstHighest=0;
+        int secondHighest=0;
+        int thirdHighest=0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > firstHighest) {
                 thirdHighest = secondHighest;
-                secondHighest = highest;
-                highest = arr[i];
+                secondHighest = firstHighest;
+                firstHighest = arr[i];
+            } else if (arr[i] > secondHighest) {
+                thirdHighest = secondHighest;
+                secondHighest = arr[i];
+            } else if (arr[i] > thirdHighest) {
+                thirdHighest = arr[i];
             }
         }
-        System.out.println("first "+highest);
-        System.out.println("second "+secondHighest);
-        System.out.println("third "+thirdHighest);
-
+        System.out.println(firstHighest);
+        System.out.println(secondHighest);
+        System.out.println(thirdHighest);
     }
+
 }
