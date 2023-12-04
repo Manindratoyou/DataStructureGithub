9c06941bd5aa282557b1f409caf4e5b2fa6f6940
manindra <manindratoyou@gmail.com>
2023-12-04 23:47:41 +0530

    leet code Solution78 subsets

---
+++ src/com/manindra/leetcodeMedium/Solution78.java
@@ -0,0 +1,41 @@
+package com.manindra.leetcodeMedium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+//Input: nums = [1,2,3]
+//Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+public class Solution78 { //subsets
+
+    public static void main(String[] args) {
+        int [] nums={1,2,3};
+        List<List<Integer>> result=subsets(nums);
+        System.out.println(result);
+    }
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+
+        // Start backtracking from the beginning
+        backtrack(resultList, new ArrayList<>(), nums, 0);
+        return resultList;
+    }
+
+    static void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet,
+                           int[] nums, int start) {
+        // Add the set to result set
+        resultSets.add(new ArrayList<>(tempSet));
+        for (int i = start; i < nums.length; i++) {
+
+            // Case of including the number
+            tempSet.add(nums[i]);
+
+            // Backtrack the new subset
+            backtrack(resultSets, tempSet, nums, i + 1);
+
+            // Case of not-including the number
+            tempSet.remove(tempSet.size() - 1);
+        }
+    }
+}
