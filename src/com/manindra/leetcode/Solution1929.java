5d3dcc45bb0a97fffe4212897186cd82bc9ae844
manindra <manindratoyou@gmail.com>
2023-12-11 17:45:58 +0530

    leet code Solution1929 Concatenation of Array

---
+++ src/com/manindra/leetcode/Solution1929.java
@@ -0,0 +1,34 @@
+package com.manindra.leetcode;
+
+/*
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+ */
+public class Solution1929 { //Concatenation of Array
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        int[] concatenatedArray = getConcatenation(nums);
+
+        System.out.print("Concatenated Array: ");
+        for (int num : concatenatedArray) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[2 * n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = nums[i];
+            result[i + n] = nums[i];
+        }
+
+        return result;
+    }
+
+}
