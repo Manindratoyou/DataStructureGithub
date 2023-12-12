edfc3871616251d2a6144c434d95bcf7320950bd
manindra <manindratoyou@gmail.com>
2023-12-12 15:44:25 +0530

    leet code Solution1512 Number of Good Pairs

---
+++ src/com/manindra/leetcode/Solution1512.java
@@ -0,0 +1,47 @@
+package com.manindra.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+ */
+public class Solution1512 { //Number of Good Pairs
+
+    public static void main(String[] args) { //Time Complexity: O(n^2) Auxiliary Space: O(1)
+
+        int[] nums = {1, 2, 3, 1, 1, 3};
+        //System.out.println("Number of good pairs: " + numIdenticalPairs(nums));
+        System.out.println("Number of good pairs: " + numIdenticalPairsSolutionTwo(nums));
+    }
+
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static int numIdenticalPairsSolutionTwo(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        int count = 0;
+
+        for (int num : nums) {
+            count += countMap.getOrDefault(num, 0);
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+
+        return count;
+    }
+
+
+}
