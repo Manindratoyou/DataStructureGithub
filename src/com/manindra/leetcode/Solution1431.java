349b974c43e752cd00714e45a14ef2da999c8705
manindra <manindratoyou@gmail.com>
2023-12-17 08:30:47 +0530

    leet code Solution1431 Kids With the Greatest Number of Candies

---
+++ src/com/manindra/leetcode/Solution1431.java
@@ -0,0 +1,60 @@
+package com.manindra.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+/*
+Input: candies = [2,3,5,1,3], extraCandies = 3
+Output: [true,true,true,false,true]
+Explanation: If you give all extraCandies to:
+- Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+- Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+- Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+- Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+- Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+ */
+public class Solution1431 { //Kids With the Greatest Number of Candies
+
+    public static void main(String[] args) {
+
+        int [] candies = {2,3,5,1,3}; int extraCandies = 3;
+        List<Boolean> result=kidsWithCandies(candies,extraCandies);
+        System.out.println(result);
+    }
+
+
+    //using stream
+    static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = getMaxCandies(candies);
+        return Arrays.stream(candies)
+                .mapToObj(candy -> candy + extraCandies >= maxCandies)
+                .collect(Collectors.toList());
+    }
+
+    static int getMaxCandies(int[] candies) {
+        return Arrays.stream(candies).max().orElse(0);
+    }
+
+    //Solution 2: Using Iteration
+    //Time Complexity: O(n) where n is the number of elements in the candies array.
+    //Auxiliary Space: O(n) for the result list.
+    public List<Boolean> kidsWithCandiesSolTwo(int[] candies, int extraCandies) {
+        int maxCandies = getMaxCandiesSolTwo(candies);
+        List<Boolean> result = new ArrayList<>();
+
+        for (int candy : candies) {
+            result.add(candy + extraCandies >= maxCandies);
+        }
+
+        return result;
+    }
+
+    private int getMaxCandiesSolTwo(int[] candies) {
+        int max = 0;
+        for (int candy : candies) {
+            max = Math.max(max, candy);
+        }
+        return max;
+    }
+}
