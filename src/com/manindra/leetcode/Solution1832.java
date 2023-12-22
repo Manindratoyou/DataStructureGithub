d700e75ca3dc5d5876511006f26af22337d919e8
manindra <manindratoyou@gmail.com>
2023-12-22 22:26:46 +0530

    leet code Solution1832 Check if the Sentence Is Pangram

---
+++ src/com/manindra/leetcode/Solution1832.java
@@ -0,0 +1,37 @@
+package com.manindra.leetcode;
+
+/*
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.
+ */
+public class Solution1832 { //Check if the Sentence Is Pangram
+
+    public static void main(String[] args) {
+
+    String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        //System.out.println(panagramCheck(sentence));
+        System.out.println(checkIfPangram(sentence));
+}
+
+    static boolean panagramCheck(String s){
+        int [] count=new int[26];
+
+        for (int i=0;i< s.length();i++){
+            count[s.charAt(i)-'a']++;
+        }
+
+        for (int i=0;i< count.length;i++){
+            if (count[i]==0)
+                return false;
+        }
+        return true;
+    }
+    static boolean checkIfPangram(String sentence) {
+        char ch ='a' ;
+        for(int i=0;i<26;i++)
+            if (sentence.contains(String.valueOf(ch))) ch++;
+            else return false;
+        return true;
+    }
+}
