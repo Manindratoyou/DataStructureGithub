53b450c4985c40b02bcb6c9784849f2ef964808b
manindra <manindratoyou@gmail.com>
2024-03-12 10:33:06 +0530

    Solution496

--- src/com/manindra/leetcode/Solution496.java
+++ src/com/manindra/leetcode/Solution496.java
@@ -7,6 +7,15 @@ import java.util.Stack;
 
 public class Solution496 { //Next Greater Element I
 
+    /*
+    Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+- 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+- 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+     */
+
     public static void main(String[] args) {
 
         int[] nums1 = {4, 1, 2};
