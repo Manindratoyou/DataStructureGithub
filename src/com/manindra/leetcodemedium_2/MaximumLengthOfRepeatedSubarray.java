08e2a5bdf6f25db7aec145d214408e359ed99f92
manindra <manindratoyou@gmail.com>
2024-04-22 11:10:50 +0530

    MaximumLengthOfRepeatedSubarray

---
+++ src/com/manindra/leetcodemedium_2/MaximumLengthOfRepeatedSubarray.java
@@ -0,0 +1,34 @@
+package com.manindra.leetcodemedium_2;
+
+public class MaximumLengthOfRepeatedSubarray {
+
+    public static void main(String[] args) {
+
+        int [] nums1 = {1,2,3,2,1}, nums2 = {3,2,1,4,7};
+        System.out.println(findLength(nums1,nums2));
+
+    }
+
+    static int findLength(int[] nums1, int[] nums2) {
+
+        int [] [] dp=new int[nums1.length+1][nums2.length+1];
+
+        int max=0;
+
+        //iterate over each position in the matrix
+        for (int i=1;i<=nums1.length;i++){
+            for (int j=1;j<= nums2.length;j++){
+
+                //if numbers are equal
+                if(nums1[i-1]==nums2[j-1]){
+                    //get the number from diagonally opposite and add 1
+                    dp[i][j]=dp[i-1][j-1]+1;
+
+                    max=Math.max(dp[i][j],max);
+                }
+            }
+        }
+        return max;
+    }
+
+}
