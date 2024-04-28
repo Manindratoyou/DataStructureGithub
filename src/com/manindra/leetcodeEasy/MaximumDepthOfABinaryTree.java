f65c763b2b53d9b990b88990c6ad6bb0e2cfecc5
manindra <manindratoyou@gmail.com>
2024-04-28 12:36:19 +0530

    MaximumDepthOfABinaryTree

---
+++ src/com/manindra/leetcodeEasy/MaximumDepthOfABinaryTree.java
@@ -0,0 +1,54 @@
+package com.manindra.leetcodeEasy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MaximumDepthOfABinaryTree { //solution 104
+
+    public static void main(String[] args) {
+        // Test cases
+        // Test Case 1: Null tree
+        MaximumDepthOfABinaryTree.TreeNode nullTree = null;
+        System.out.println("Test Case 1: " + MaximumDepthOfABinaryTree.maxDepth(nullTree));
+
+        // Test Case 2: Single node tree
+        MaximumDepthOfABinaryTree.TreeNode singleNodeTree = new MaximumDepthOfABinaryTree.TreeNode(5);
+        System.out.println("Test Case 2: " + MaximumDepthOfABinaryTree.maxDepth(singleNodeTree));
+
+        // Test Case 3: Provided tree [3,9,20,null,null,15,7]
+        MaximumDepthOfABinaryTree.TreeNode root = new MaximumDepthOfABinaryTree.TreeNode(3);
+        root.left = new MaximumDepthOfABinaryTree.TreeNode(9);
+        root.right = new MaximumDepthOfABinaryTree.TreeNode(20);
+        root.right.left = new MaximumDepthOfABinaryTree.TreeNode(15);
+        root.right.right = new MaximumDepthOfABinaryTree.TreeNode(7);
+
+        System.out.println("Test Case 3: " + MaximumDepthOfABinaryTree.maxDepth(root));
+    }
+
+    static int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
