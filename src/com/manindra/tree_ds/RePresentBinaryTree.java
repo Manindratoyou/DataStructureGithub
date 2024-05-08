2fbd356c9c6145e84f1f07e7bfe650812b250a9d
manindra <manindratoyou@gmail.com>
2024-05-08 10:01:55 +0530

    RePresentBinaryTree

---
+++ src/com/manindra/tree_ds/RePresentBinaryTree.java
@@ -0,0 +1,41 @@
+package com.manindra.tree_ds;
+
+public class RePresentBinaryTree {
+
+    public static void main(String[] args) {
+
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+
+        // Print the binary tree
+        System.out.println("Binary Tree Representation:");
+        printBinaryTree(root);
+    }
+
+    // Method to print binary tree
+    static void printBinaryTree(Node node) {
+        if (node == null)
+            return;
+
+        // Print the node value
+        System.out.print(node.key + " ");
+
+        // Recursively print left and right subtrees
+        printBinaryTree(node.left);
+        printBinaryTree(node.right);
+    }
+
+    static class Node {
+
+        int key;
+        Node left;
+        Node right;
+
+        Node(int k) {
+            key = k;
+            left = right = null;
+        }
+    }
+}
