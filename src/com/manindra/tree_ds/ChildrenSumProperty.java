a4a6a842b0c769e3084cb9d07b9324fb73b11ecf
manindra <manindratoyou@gmail.com>
2024-05-12 23:17:04 +0530

    ChildrenSumProperty

---
+++ src/com/manindra/tree_ds/ChildrenSumProperty.java
@@ -0,0 +1,52 @@
+package com.manindra.tree_ds;
+
+public class ChildrenSumProperty {
+
+    public static void main(String[] args) {
+
+        /*
+
+        20
+       /  \
+      8    12
+     / \     \
+    3   5    12
+
+     */
+
+
+        Node root = new Node(20);
+        root.left = new Node(8);
+        root.right = new Node(12);
+        root.left.left = new Node(3);
+        root.left.right = new Node(5);
+        root.right.right = new Node(12);
+        System.out.println(isSum(root));
+
+    }
+
+    static boolean isSum(Node root) {
+        if (root == null)
+            return true;
+        if (root.left == null && root.right == null)
+            return true;
+        int sum = 0;
+        if (root.left != null)
+            sum += root.left.key;
+        if (root.right != null)
+            sum += root.right.key;
+        return root.key == sum && isSum(root.left) && isSum(root.right);
+    }
+
+    static class Node {
+
+        int key;
+        Node left;
+        Node right;
+
+        Node(int k) {
+            key = k;
+            left = right = null;
+        }
+    }
+}
