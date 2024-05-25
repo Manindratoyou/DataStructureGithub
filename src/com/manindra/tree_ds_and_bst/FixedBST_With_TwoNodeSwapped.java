5baa35e3ece1786ef5e67371e899365c7a1431c7
manindra <manindratoyou@gmail.com>
2024-05-25 18:19:22 +0530

    FixedBST_With_TwoNodeSwapped

---
+++ src/com/manindra/tree_ds_and_bst/FixedBST_With_TwoNodeSwapped.java
@@ -0,0 +1,57 @@
+package com.manindra.tree_ds_and_bst;
+
+public class FixedBST_With_TwoNodeSwapped {
+
+    public static void main(String args[])
+    {
+        Node root = new Node(18);
+        root.left = new Node(60);
+        root.right = new Node(70);
+        root.left.left = new Node(4);
+        root.right.left = new Node(8);
+        root.right.right = new Node(80);
+
+        inorder(root);
+        System.out.println();;
+        fixBST(root);
+        int temp=first.key;
+        first.key=second.key;
+        second.key=temp;
+        inorder(root);
+    }
+
+    public static void inorder(Node root){
+        if(root!=null){
+            inorder(root.left);
+            System.out.print(root.key+" ");
+            inorder(root.right);
+        }
+    }
+
+    static Node prev=null,first=null,second=null;
+    public static void fixBST(Node root)
+    {
+        if (root == null)
+            return;
+        fixBST(root.left);
+        if(prev!=null && root.key<prev.key){
+            if(first==null)
+                first=prev;
+            second=root;
+        }
+        prev=root;
+
+        fixBST(root.right);
+    }
+
+
+    static class Node {
+        int key;
+        Node left, right;
+        Node(int x)
+        {
+            key = x;
+            left = right = null;
+        }
+    }
+}
