b0cc2bc821696d065c9f69c163487245f87a47b2
manindra <manindratoyou@gmail.com>
2024-06-03 11:15:14 +0530

    MedianOfAStream

---
+++ src/com/manindra/heap_ds/MedianOfAStream.java
@@ -0,0 +1,32 @@
+package com.manindra.heap_ds;
+
+public class MedianOfAStream {
+
+    public static void main(String[] args) {
+
+        double [] arr={25,7,10,15,20};
+        median(arr);
+    }
+
+    static void median(double[] arr) {
+        int n= arr.length;
+        double[] temp=new double[n];
+        int size=0;
+        for(int i=0;i<n;i++){
+            temp[i]=arr[i];
+            size++;
+            int j=i;
+            while(j>0&&temp[j]<temp[j-1]){
+                double a=temp[j];
+                temp[j]=temp[j-1];
+                temp[j-1]=a;
+                j--;
+            }
+            if(size%2!=0)
+                System.out.print(temp[size/2]+" ");
+            else
+                System.out.print(((temp[size/2]+temp[(size-1)/2])/2)+" ");
+
+        }
+    }
+}
