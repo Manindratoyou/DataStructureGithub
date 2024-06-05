3515c32b6463f21e00b710e1e31da476a4a44d30
manindra <manindratoyou@gmail.com>
2024-06-05 11:50:09 +0530

    StockSpan2

---
+++ src/com/manindra/leetcodemedium_2/StockSpan2.java
@@ -0,0 +1,39 @@
+package com.manindra.leetcodemedium_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockSpan2 { //901
+
+    public static void main(String[] args) {
+
+        StockSpan2 stockSpan = new StockSpan2();
+        System.out.println(stockSpan.next(100));
+        System.out.println(stockSpan.next(80));
+        System.out.println(stockSpan.next(60));
+        System.out.println(stockSpan.next(70));
+        System.out.println(stockSpan.next(60));
+        System.out.println(stockSpan.next(75));
+        System.out.println(stockSpan.next(85));
+    }
+
+    private List<Integer> list;
+
+    public StockSpan2() {
+        list = new ArrayList<>();
+    }
+
+    public int next(int price) {
+        list.add(price);
+        int count = 0;
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (list.get(i) <= price) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+}
