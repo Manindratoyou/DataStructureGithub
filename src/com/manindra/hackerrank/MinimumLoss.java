a70ce458b12b0f46530202e49fe611c03b754c8d
manindra <manindratoyou@gmail.com>
2024-06-05 11:47:49 +0530

    MinimumLoss Hacker rank

--- src/com/manindra/hackerrank/MinimumLoss.java
+++ src/com/manindra/hackerrank/MinimumLoss.java
@@ -11,6 +11,7 @@ public class MinimumLoss {
         System.out.println(minimumLoss(Arrays.asList(20L, 15L, 8L, 2L, 12L)));
     }
 
+
     static void minLoss(int[] prices) {
         int minLoss = Integer.MAX_VALUE;
         for (int i = 0; i < prices.length; i++) {
