57980f0597b425c9278354301d0a30aafec09ecb
manindra <manindratoyou@gmail.com>
2024-06-06 16:49:28 +0530

    updated code

--- src/com/manindra/leetcode/Solution1672.java
+++ src/com/manindra/leetcode/Solution1672.java
@@ -56,4 +56,19 @@ public class Solution1672 { //Richest Customer Wealth
                 .orElse(0);
     }
 
+    static int maximumWealth3(int[][] accounts) {
+
+        int max = 0;
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum = sum + accounts[i][j];
+            }
+            /*if (sum>max)
+                max=sum;*/
+            max = Math.max(sum, max);
+        }
+        return max;
+    }
+
 }
--- src/com/manindra/leetcode/Solution205.java
+++ src/com/manindra/leetcode/Solution205.java
@@ -6,62 +6,87 @@ import java.util.Map;
 public class Solution205 { //Isomorphic String
 
     public static void main(String[] args) {
-        String s1="egg";
-        String t1="add";
+        String s1 = "egg";
+        String t1 = "add";
 
-        String s2="foo";
-        String t2="bar";
+        String s2 = "foo";
+        String t2 = "bar";
 
-        System.out.println(isIsomorphic(s1,t1));
-        System.out.println(isIsomorphicSolutionTwo(s2,t2));
+        System.out.println(isIsomorphic(s1, t1));
+        System.out.println(isIsomorphicSolutionTwo(s2, t2));
 
     }
 
-    static boolean isIsomorphic(String s,String t){
-        Map<Character,Character> sMap=new HashMap<>();
-        Map<Character,Character> tMap=new HashMap<>();
+    static boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> sMap = new HashMap<>();
+        Map<Character, Character> tMap = new HashMap<>();
 
-        for (int i=0;i<s.length();i++){
-            char sChar=s.charAt(i);
-            char tChar=t.charAt(i);
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
 
-            if (sMap.containsKey(sChar)){
-                if (sMap.get(sChar)!=tChar)
+            if (sMap.containsKey(sChar)) {
+                if (sMap.get(sChar) != tChar)
                     return false;
-            }else {
-                tMap.put(tChar,sChar);
+            } else {
+                tMap.put(tChar, sChar);
             }
 
-            if (tMap.containsKey(tChar)){
-                if (tMap.get(tChar)!=sChar)
+            if (tMap.containsKey(tChar)) {
+                if (tMap.get(tChar) != sChar)
                     return false;
-            }else {
-                tMap.put(tChar,sChar);
+            } else {
+                tMap.put(tChar, sChar);
             }
         }
         return true;
     }
-    static boolean isIsomorphicSolutionTwo(String s,String t){
-        if (s.length() !=t.length()) return false;
 
-        int [] sCharMap=new int[256];//mapping array for characters in string 's'
-        int [] tCharMap=new int[256];//mapping array for characters in string 't'
+    static boolean isIsomorphicSolutionTwo(String s, String t) {
+        if (s.length() != t.length()) return false;
 
-        for (int i=0;i<s.length();i++){
-            char sChar=s.charAt(i);
-            char tChar=t.charAt(i);
+        int[] sCharMap = new int[256];//mapping array for characters in string 's'
+        int[] tCharMap = new int[256];//mapping array for characters in string 't'
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
 
             //check the character mappings are different in 's' and 't'
-            if (sCharMap[sChar] !=tCharMap[tChar]) return false;
+            if (sCharMap[sChar] != tCharMap[tChar]) return false;
 
             //update the character mapping in 's' and 't'
-            if (sCharMap[sChar]==0)
-                sCharMap[sChar] =i+1;//increment by 1 to avoid index 0 conflict.
-            if (tCharMap[tChar]==0)
-                tCharMap[tChar]=i+1;//increment by 1 to avoid index 0 conflict
+            if (sCharMap[sChar] == 0)
+                sCharMap[sChar] = i + 1;//increment by 1 to avoid index 0 conflict.
+            if (tCharMap[tChar] == 0)
+                tCharMap[tChar] = i + 1;//increment by 1 to avoid index 0 conflict
 
         }
 
+        return true;
+    }
+
+    static boolean isIsomorphic3(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        //create a hashmap to store character mapping
+        Map<Character, Character> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char original = s.charAt(i);
+            char replacement = t.charAt(i);
+
+            if (!map.containsKey(original)) {
+                if (map.containsValue(replacement))
+                    map.put(original, replacement);
+                else
+                    return false;
+            } else {
+                char mapCharacter = map.get(original);
+                if (mapCharacter != replacement)
+                    return false;
+            }
+        }
         return true;
     }
 
