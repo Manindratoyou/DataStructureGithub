7ed61b280f7f8c5beb2f8eef0503d4547b2a00be
manindrak <manindratoyou@gmail.com>
2024-11-29 13:19:44 +0530

    updated code

--- src/com/manindra/leetcodeEasy/AverageOfLevelsBinaryTree.java
+++ src/com/manindra/leetcodeEasy/AverageOfLevelsBinaryTree.java
@@ -9,49 +9,23 @@ public class AverageOfLevelsBinaryTree { //leet code solution 637
 
     public static void main(String[] args) {
 
-        // Creating a tree that looks like
-        //            2
-        TreeNode root = new TreeNode(2);
+        /*
+          3
+         / \
+        9   20
+           /  \
+          15   7
 
-        // Adding left and right child
-        //            2
-        //          /  \
-        //        8     4
-        root.left = new TreeNode(8);
-        root.right = new TreeNode(4);
+         */
 
-        TreeNode leftNode = root.left;
-        TreeNode rightNode = root.right;
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
 
-        // Adding more child to left node
-        //            2
-        //          /  \
-        //        8     4
-        //      /  \
-        //     3   7
-        leftNode.left = new TreeNode(3);
-        leftNode.right = new TreeNode(7);
-
-        // Adding child to right node
-        //            2
-        //          /  \
-        //        8     4
-        //      /  \     \
-        //     3   7      1
-        rightNode.right = new TreeNode(1);
-
-        // Adding child to child of right node
-        //            2
-        //          /  \
-        //        8     4
-        //      /  \     \
-        //     3   7      1
-        //               /
-        //              6
-        rightNode.right.left = new TreeNode(6);
-
-        System.out.println("level order traversal "+levelOrder(root));
-        System.out.println("average of levels  "+averageOfLevels(root));
+        //System.out.println("level order traversal "+levelOrder(root));
+        System.out.println("average of levels  " + averageOfLevels(root));
 
     }
 
