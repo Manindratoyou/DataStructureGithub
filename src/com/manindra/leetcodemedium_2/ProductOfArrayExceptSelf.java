34e46e81a013de873a6ef90cd11842916a682b0b
manindrak <manindratoyou@gmail.com>
2024-12-04 22:01:30 +0530

    updated code

--- src/com/manindra/leetcodemedium_2/ProductOfArrayExceptSelf.java
+++ src/com/manindra/leetcodemedium_2/ProductOfArrayExceptSelf.java
@@ -7,8 +7,11 @@ public class ProductOfArrayExceptSelf {
     public static void main(String[] args) {
 
         int[] nums = {1, 2, 3, 4};
+        int[] nums1 = {-1, 1, 0, -3, 3};
         int[] result = productExceptSelf(nums);
+        int[] result1 = productExceptSelf(nums1);
         System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(result1));
     }
 
     static int[] productExceptSelf(int[] nums) {
