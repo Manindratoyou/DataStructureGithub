a87348ca2b083aaa6e71cb3f2ceb458e12daf53c
manindrak <manindratoyou@gmail.com>
2024-12-09 15:01:29 +0530

    updated code

--- src/com/manindra/leetcode/Solution125.java
+++ src/com/manindra/leetcode/Solution125.java
@@ -1,25 +1,25 @@
 package com.manindra.leetcode;
 
-public class Solution125 {
+public class Solution125 { //Valid Palindrome
 
     public static void main(String[] args) {
 
-        String input1="A man , a plan , a canal : Panama";
-        String input2="race a car";
+        String input1 = "A man , a plan , a canal : Panama";
+        String input2 = "race a car";
         System.out.println(isPalindrome(input1));
         System.out.println(isPalindrome(input2));
     }
 
-    static boolean isPalindrome(String input){
+    static boolean isPalindrome(String input) {
 
-        if (input==null || input.length()==0) return true;
+        if (input == null || input.length() == 0) return true;
 
-        input=input.replaceAll("[^a-zA-Z0-9]","").toLowerCase(); //amanaplanacanalpanama
-        int left=0;
-        int right=input.length()-1;
+        input = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase(); //amanaplanacanalpanama
+        int left = 0;
+        int right = input.length() - 1;
 
-        while (left<right){
-            if (input.charAt(left)!=input.charAt(right)) return false;
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) return false;
             left++;
             right--;
 
