cbe6563ed271b9fe913a29dc0bbe30e27a3f42fd
manindra <manindratoyou@gmail.com>
2025-02-24 00:28:33 +0530

    updated code

--- src/com/manindra/leetcodeMedium/Solution53.java
+++ src/com/manindra/leetcodeMedium/Solution53.java
@@ -5,6 +5,7 @@ public class Solution53 { //max sum sub array
     public static void main(String[] args) {
         int [] arr={2,3,-8,7,-1,2,3};
         System.out.println(maxSumSubArray(arr,arr.length));
+        System.out.println(maxSubArray(arr));
     }
 
     static int maxSumSubArray(int [] arr, int n){
@@ -16,4 +17,17 @@ public class Solution53 { //max sum sub array
         }
         return result;
     }
+    static int maxSubArray(int[] nums) {
+
+        int max_so_far = nums[0];
+        int curr_max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            curr_max = Math.max(nums[i], nums[i] + curr_max);
+            max_so_far = Math.max(curr_max, max_so_far);
+        }
+
+        return max_so_far;
+    }
+
 }
