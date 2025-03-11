e6021cb9426e2801bb526daf07d141c872095969
manindrak <manindratoyou@gmail.com>
2025-03-11 18:53:33 +0530

    updated code

--- src/com/manindra/leetcode/Solution121.java
+++ src/com/manindra/leetcode/Solution121.java
@@ -7,6 +7,7 @@ public class Solution121 { //best time to buy and sell stocks
         int [] prices={7,1,5,3,6,4};
         System.out.println("Maximum Profit = " +maxProfit(prices));
         System.out.println("Maximum Profit = " +maxProfitSolutionTwo(prices));
+        System.out.println("Maximum Profit = " +maxProfit3(prices));
     }
 
     static int maxProfit(int [] prices){
@@ -37,6 +38,30 @@ public class Solution121 { //best time to buy and sell stocks
                 maxProfit=Math.max(maxProfit,prices[i]-minPrice);
         }
         return maxProfit;
+    }
+
+    static int maxProfit3(int[] prices) {
+
+        // at the beginning the minimum price is the first price
+        int buy_price = prices[0];
+
+        // at the beginning the minimum  profit is zero
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            // if the current price is less update the buy_price
+            if (prices[i] < buy_price) {
+                buy_price = prices[i];
+            }
+            else {
+                // else check if we can get a better profit
+                int current_profit = prices[i] - buy_price;
+                profit = Math.max(current_profit, profit);
+            }
+        }
+
+        return profit;
     }
 
 }
