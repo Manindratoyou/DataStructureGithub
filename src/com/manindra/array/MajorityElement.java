022a509f80b0d7a3a19c99df082fa114628dfafc
manindrak <manindratoyou@gmail.com>
2025-03-17 15:32:38 +0530

    updated code

--- src/com/manindra/array/MajorityElement.java
+++ src/com/manindra/array/MajorityElement.java
@@ -4,14 +4,17 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MajorityElement {
+public class MajorityElement { //solution 169
 
     public static void main(String[] args) {
         int [] arr={8,3,4,8,8};
         System.out.println(findMajority(arr, arr.length));//majority element more than n/2 times
-        System.out.println(findMajoritySolTwo(arr, arr.length));
+        System.out.println("=================");
+        int[] nums1 = {2,2,1,3,1,2,2};
+        System.out.println(findMajoritySolTwo(nums1));
         System.out.println("=================");
         int[] nums = {3, 3, 4, 2, 4, 4, 2, 4, 4};
+
         int majorityElement = findMajorityElementSolFour(nums);
         if (majorityElement != -1) {
             System.out.println("Majority Element: " + majorityElement);
@@ -31,31 +34,22 @@ public class MajorityElement {
         }
         return -1;
     }
-    static int findMajoritySolTwo(int [] arr,int n){
-        int res = 0, count = 1;
+    static int findMajoritySolTwo(int[] num) {
 
-        for(int i = 1; i < n; i++)
-        {
-            if(arr[res] == arr[i])
-                count++;
-            else
-                count --;
+        int majority = num[0], votes = 1;
 
-            if(count == 0)
-            {
-                res = i; count = 1;
-            }
+        for (int i = 1; i < num.length; i++) {
+
+            if (votes == 0) {
+                votes++;
+                majority = num[i];
+            } else if (majority == num[i]) {
+                votes++;
+            } else
+                votes--;
+
         }
-
-        count = 0;
-        for(int i = 0; i < n; i++)
-            if(arr[res] == arr[i])
-                count++;
-
-        if(count <= n /2)
-            res = -1;
-
-        return res;
+        return majority;
     }
 
     static int findMajorityElementSolThree(int[] nums) {
@@ -73,13 +67,13 @@ public class MajorityElement {
     }
 
     static int findMajorityElementSolFour(int[] nums) {
-        Map<Integer, Integer> countMap = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
         for (int num : nums) {
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             if (entry.getValue() > nums.length / 2) {
                 return entry.getKey();
             }
