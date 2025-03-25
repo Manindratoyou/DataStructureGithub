2fa727ac4c38eb40813073b6225971a50e164134
manindrak <manindratoyou@gmail.com>
2025-03-25 20:14:48 +0530

    updated solution

--- src/com/manindra/leetcode/Solution268.java
+++ src/com/manindra/leetcode/Solution268.java
@@ -8,11 +8,15 @@ public class Solution268 { //missing number
     public static void main(String[] args) {
         // Example Input:
         int[] nums = {3, 0, 1};
+        int[] num1 = {9,6,4,2,3,5,7,0,1};
 
         // Calculate and print the missing number
         //int result = missingNumber(nums);
         int result = missingNumberSolutionTwo(nums);
         System.out.println("Missing number: " + result);
+        System.out.println("==================");
+        int result2 = missingNumber3(num1);
+        System.out.println("Missing number: " + result2);
     }
 
     public static int missingNumber(int[] nums) {
@@ -39,5 +43,37 @@ public class Solution268 { //missing number
         }
 
         return expectedSum - actualSum;
+    }
+
+    /*
+    Properties of XOR
+
+   Commutative
+   A ⊕ B = B ⊕ A
+
+   A ⊕ B ⊕ C ⊕ D = A ⊕ D ⊕ C ⊕ B
+
+  XOR with 0
+  A ⊕ 0 = A
+
+  XOR with self
+  A ⊕ A = 0
+     */
+    static int missingNumber3(int[] nums) {
+
+        int allXOR = 0;
+
+        // XOR all numbers in range [0, n]
+        for (int i = 0; i <= nums.length; i++) {
+            allXOR = allXOR ^ i;
+        }
+
+        // XOR all numbers in the given array
+        for (int num : nums) {
+            allXOR = allXOR ^ num;
+        }
+
+        // The missing number
+        return allXOR;
     }
 }
