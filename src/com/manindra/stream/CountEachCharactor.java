389c96fd13686f8f67125382987c083a46019e9b
manindra <manindratoyou@gmail.com>
2025-03-26 11:09:05 +0530

    updated code

--- src/com/manindra/stream/CountEachCharactor.java
+++ src/com/manindra/stream/CountEachCharactor.java
@@ -20,12 +20,16 @@ public class CountEachCharactor {
     /*public static void main(String[] args) {
         String s = "string data to count each character";
 
-        LinkedHashMap<String, Long> wordCount = Arrays.stream(s.split(" "))
-                .map(String::toLowerCase)
-                .collect(Collectors.groupingBy(
-                        st -> st, LinkedHashMap::new, Collectors.counting()
-                ));
+        LinkedHashMap<String, Long> wordCount =
+                Arrays.stream(s.split(" ")) // Step 1: Convert string into a stream of words
+                        .map(String::toLowerCase) // Step 2: Convert each word to lowercase
+                        .collect(Collectors.groupingBy(
+                                st -> st, // Step 3: Use the word as the key
+                                LinkedHashMap::new, // Step 4: Use a LinkedHashMap to maintain order
+                                Collectors.counting() // Step 5: Count occurrences of each word
+                        ));
 
         System.out.println(wordCount);
-    }*/
+        }
+        */
 }
