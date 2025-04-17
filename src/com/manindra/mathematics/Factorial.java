061be50cdddf785fb66d0a25f74c3a42d69eb39c
manindrak <manindratoyou@gmail.com>
2025-04-17 16:33:01 +0530

    updated code

--- src/com/manindra/mathematics/Factorial.java
+++ src/com/manindra/mathematics/Factorial.java
@@ -8,17 +8,17 @@ public class Factorial {
         System.out.println(factRecursive(5));
     }
 
-    static int factIterative(int number){
-        int res=1;
-        for(int i=1;i<=number;i++){
-            res=res*i;
+    static int factIterative(int number) {
+        int res = 1;
+        for (int i = 1; i <= number; i++) {
+            res = res * i;
         }
         return res;
     }
 
-    static int factRecursive(int n){
-        if (n==0)
+    static int factRecursive(int n) {
+        if (n == 0)
             return 1;
-        return n*factRecursive(n-1);
+        return n * factRecursive(n - 1);
     }
 }
--- src/com/manindra/mathematics/NumberPalindrome.java
+++ src/com/manindra/mathematics/NumberPalindrome.java
@@ -4,17 +4,24 @@ public class NumberPalindrome {
 
     public static void main(String[] args) {
         System.out.println(palindrome(363));
-        System.out.println(3%10);
-        System.out.println(3/10);
+        System.out.println(palindrome(78987));
+        System.out.println(palindrome(8));
+        System.out.println(palindrome(89));
+        System.out.println("-------------------------");
+        System.out.println(3 % 10);
+        System.out.println(3 / 10);
+        System.out.println(13 % 10);
+        System.out.println(13 / 10);
     }
-    static boolean palindrome(int number){
-        int rev=0;
-        int temp=number;
-        while (temp!=0){
-            int ld=temp%10;
-            rev=rev*10+ld;
-            temp=temp/10;
+
+    static boolean palindrome(int number) {
+        int rev = 0;
+        int temp = number;
+        while (temp != 0) {
+            int ld = temp % 10;
+            rev = rev * 10 + ld;
+            temp = temp / 10;
         }
-        return rev==number;
+        return rev == number;
     }
 }
