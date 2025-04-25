28016cce779aef7b417f1d7ad500af5daa70ebd1
manindrak <manindratoyou@gmail.com>
2025-04-25 22:54:21 +0530

    updated code

--- src/com/manindra/leetcode/Solution643.java
+++ src/com/manindra/leetcode/Solution643.java
@@ -4,9 +4,11 @@ public class Solution643 { //Maximum Average Sub array I (sliding Window Techniq
 
     public static void main(String[] args) {
 
-        int [] arr={1,12,-5,-6,50,3};
-        System.out.println(findMaxAverage(arr,4));
+        int[] arr = {1, 12, -5, -6, 50, 3};
+        System.out.println(findMaxAverage(arr, 4));
+        System.out.println(findMaxAverage2(arr, 4));
     }
+
     static double findMaxAverage(int[] nums, int k) {
         // Get sum for starting window
         int sum = 0;
@@ -31,5 +33,20 @@ public class Solution643 { //Maximum Average Sub array I (sliding Window Techniq
 
         // Return the average
         return (double) maxSum / k;
+    }
+
+    static double findMaxAverage2(int[] nums, int k) {
+
+        double ans = 0;
+        double window = 0;
+        for (int i = 0; i < k; i++) {
+            window += nums[i];
+        }
+        ans = window / k;
+        for (int right = k; right < nums.length; right++) {
+            window += nums[right] - nums[right - k];
+            ans = Math.max(ans, window / k);
+        }
+        return ans;
     }
 }
