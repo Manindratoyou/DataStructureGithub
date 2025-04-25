661635826fd195c0467ee5c8a05ad3024b1c7c67
manindrak <manindratoyou@gmail.com>
2025-04-25 16:12:08 +0530

    updated solution

--- src/com/manindra/leetcode/Solution1832.java
+++ src/com/manindra/leetcode/Solution1832.java
@@ -1,5 +1,7 @@
 package com.manindra.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 /*
@@ -11,27 +13,30 @@ public class Solution1832 { //Check if the Sentence Is Pangram
 
     public static void main(String[] args) {
 
-    String sentence = "thequickbrownfoxjumpsoverthelazydog";
-        //System.out.println(panagramCheck(sentence));
-        System.out.println(checkIfPangram(sentence));
-}
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        String sentence1 = "The quick brown fox jumps over the lazy dog";
+        String sentence2 = "Hello World";
+        System.out.println(panagramCheck(sentence));
+        System.out.println(checkIfPangram4(sentence2));
+    }
 
-    static boolean panagramCheck(String s){
-        int [] count=new int[26];
+    static boolean panagramCheck(String s) {
+        int[] count = new int[26];
 
-        for (int i=0;i< s.length();i++){
-            count[s.charAt(i)-'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
         }
 
-        for (int i=0;i< count.length;i++){
-            if (count[i]==0)
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == 0)
                 return false;
         }
         return true;
     }
+
     static boolean checkIfPangram(String sentence) {
-        char ch ='a' ;
-        for(int i=0;i<26;i++)
+        char ch = 'a';
+        for (int i = 0; i < 26; i++)
             if (sentence.contains(String.valueOf(ch))) ch++;
             else return false;
         return true;
@@ -40,5 +45,21 @@ public class Solution1832 { //Check if the Sentence Is Pangram
     static boolean checkIfPangramStream(String sentence) {
         return IntStream.rangeClosed('a', 'z')
                 .allMatch(ch -> sentence.contains(String.valueOf((char) ch)));
+    }
+
+    static boolean checkIfPangram4(String sentence) {
+        Set<Character> set = new HashSet<>();
+
+        //add all character from 'a' to 'z' in hashset
+        for (int i = 'a'; i <= 'z'; i++) {
+            set.add((char) i);
+        }
+        //remove every character and check if set becomes empty at any point of time
+        for (int i = 0; i < sentence.length(); i++) {
+            set.remove(sentence.charAt(i));
+            if (set.isEmpty())
+                return true;
+        }
+        return false;
     }
 }
