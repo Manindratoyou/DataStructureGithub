c3c4a4266bda9966a0d4dd0759544979eee52cce
manindra <manindratoyou@gmail.com>
2025-04-25 10:30:51 +0530

    updated code

--- src/com/manindra/corejava/SingletonExampleTwo.java
+++ src/com/manindra/corejava/SingletonExampleTwo.java
@@ -1,35 +1,38 @@
 package com.manindra.corejava;
 
-class SingletonEagar{
-    private static SingletonEagar instance=new SingletonEagar();
+class SingletonEagar {
+    private static SingletonEagar instance = new SingletonEagar();
 
-    private SingletonEagar(){}
+    private SingletonEagar() {
+    }
 
-    public static SingletonEagar getInstance(){
+    public static SingletonEagar getInstance() {
         return instance;
     }
 }
-class SingletonLazy{
+
+class SingletonLazy {
     private static SingletonLazy instance;
 
-    private SingletonLazy(){}
+    private SingletonLazy() {
+    }
 
-    public static SingletonLazy getInstance(){
-        if (instance==null)
-            instance=new SingletonLazy();
+    public static SingletonLazy getInstance() {
+        if (instance == null)
+            instance = new SingletonLazy();
         return instance;
     }
 }
+
 public class SingletonExampleTwo {
 
     public static void main(String[] args) {
+        SingletonEagar instance1 = SingletonEagar.getInstance();
+        System.out.println(instance1 + " : Singleton Eager initialization");
 
-        SingletonEagar instance1=SingletonEagar.getInstance();
-        System.out.println(instance1+" : Singleton Eager initialization");
-
-        SingletonEagar instance2=SingletonEagar.getInstance();
-        System.out.println(instance2+" : Singleton Lazy initialization");
-
+        SingletonLazy instance2 = SingletonLazy.getInstance();
+        System.out.println(instance2 + " : Singleton Lazy initialization");
     }
+
 
 }
