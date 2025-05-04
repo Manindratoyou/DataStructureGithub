19267df901f274c87f83f53cae438610baae8268
manindrak <manindratoyou@gmail.com>
2025-05-04 23:21:58 +0530

    updated code

--- src/com/manindra/string/SubsequenceString.java
+++ src/com/manindra/string/SubsequenceString.java
@@ -6,32 +6,46 @@ All subsequence of ABC are : "", "A", "B", "C", "AB", "AC", "BC", "ABC"
 public class SubsequenceString {
 
     public static void main(String[] args) {
-        System.out.println(isSubSeqIterative("ABCD","AD"));
-        System.out.println(isSubSeqRecursive("ABCD","AD",4,2));
+        System.out.println(isSubSeqIterative("ABCD", "AD"));
+        System.out.println(isSubSeqRecursive("ABCD", "AD", 4, 2));
     }
 
-    static boolean isSubSeqIterative(String s1,String s2){ //tc 0(m+n) as 0(1)
-        int n=s1.length();
-        int m=s2.length();
+    static boolean isSubSeqIterative(String s1, String s2) { //tc 0(m+n) as 0(1)
+        int n = s1.length();
+        int m = s2.length();
         /*if (n<m){
             return false;
         }*/
         int j = 0;
-        for (int i=0;i<n && j<m;i++){
-            if (s1.charAt(i)==s2.charAt(j))
+        for (int i = 0; i < n && j < m; i++) {
+            if (s1.charAt(i) == s2.charAt(j))
                 j++;
         }
-        return (j==m);
+        return (j == m);
     }
-    static boolean isSubSeqRecursive(String s1,String s2,int n,int m){ //tc & as-0(m+n)
 
-        if (m==0)
+    static boolean isSubSeqRecursive(String s1, String s2, int n, int m) { //tc & as-0(m+n)
+
+        if (m == 0)
             return true;
-        if (n==0)
+        if (n == 0)
             return false;
-        if (s1.charAt(n-1)==s2.charAt(m-1))
-            return isSubSeqRecursive(s1,s2,n-1,m-1);
+        if (s1.charAt(n - 1) == s2.charAt(m - 1))
+            return isSubSeqRecursive(s1, s2, n - 1, m - 1);
         else
-            return isSubSeqRecursive(s1,s2,n-1,m);
+            return isSubSeqRecursive(s1, s2, n - 1, m);
+    }
+
+    public static boolean isSubsequence(String s1, String s2) {
+        int i = 0, j = 0;
+
+        while (i < s1.length() && j < s2.length()) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+
+        return j == s2.length();
     }
 }
