3e5230f52dcac1843b74312787cbdedcafb12a88
manindra <manindratoyou@gmail.com>
2025-05-05 00:58:40 +0530

    updated code

--- src/com/manindra/leetcodeMedium/Solution647.java
+++ src/com/manindra/leetcodeMedium/Solution647.java
@@ -12,27 +12,31 @@ Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 public class Solution647 { //Palindromic Substrings
 
     public static void main(String[] args) {
-        System.out.println(countSubstrings("abc"));
+        System.out.println(countSubstrings("abc"));//'a','b','c'
+        System.out.println(countSubstrings("aaa"));//'a','a','a','aa','aa','aaa'
+        System.out.println(countSubstrings("abbac"));//'a','b','b','a','c','bb','abba'
     }
 
     static int countSubstrings(String s) {
-        if(s.length()==0) return 0;
-        int n=s.length();
-        int res=0;
-        char[] c=s.toCharArray();
-        for(int i=0;i<n;i++){
+        if (s.length() == 0) return 0;
+        int n = s.length();
+        int res = 0;
+        char[] c = s.toCharArray();
+        for (int i = 0; i < n; i++) {
             //odd and even lengths(center for odd -> i,i)
             //center for even ->i ,i+1
-            res+=isPalindrome(i,i,c);
-            res+=isPalindrome(i,i+1,c);
+            res += isPalindrome(i, i, c);
+            res += isPalindrome(i, i + 1, c);
         }
         return res;
     }
-    static int isPalindrome(int s, int e, char[] c){
-        int count=0;
-        while(s>=0 && e<c.length && c[s--]==c[e++])
-            count++;
 
-        return count;
+    static int isPalindrome(int left, int right, char[] charArray) {
+        int result = 0;
+        while (left >= 0 && right < charArray.length &&
+                charArray[left--] == charArray[right++]) {
+            result++;
+        }
+        return result;
     }
 }
