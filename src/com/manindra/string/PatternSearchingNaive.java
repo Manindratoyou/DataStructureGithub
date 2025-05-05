0d46bf4989e31933f83bfde768d3715eaece5f46
manindrak <manindratoyou@gmail.com>
2025-05-05 16:26:15 +0530

    updated code

--- src/com/manindra/string/PatternSearchingNaive.java
+++ src/com/manindra/string/PatternSearchingNaive.java
@@ -3,37 +3,43 @@ package com.manindra.string;
 public class PatternSearchingNaive {
 
     public static void main(String[] args) {
-        String text="ABABABCD", Pattern="ABAB";
-        patternSearchingNaive(text,Pattern);
-        //patternSearchingNaiveImprove(text,Pattern);
+        String text = "ABABABCD", Pattern = "ABAB";
+        String text1 = "ABABABCD", Pattern1 = "AB";
+        patternSearchingNaive(text, Pattern);
+        System.out.println("--------------");
+        searchPattern2(text1, Pattern1);
     }
 
-    static void patternSearchingNaive(String text,String pattern){
-        int m=pattern.length();
-        int n=text.length();
-        for (int i=0;i<n-m;i++){
+    static void patternSearchingNaive(String text, String pattern) {
+        int m = pattern.length();
+        int n = text.length();
+        for (int i = 0; i < n - m; i++) {
             int j;
-            for (j=0;j<m;j++)
-                if (pattern.charAt(j)!=text.charAt(i+j))
+            for (j = 0; j < m; j++)
+                if (pattern.charAt(j) != text.charAt(i + j))
                     break;
-            if (j==m)
-                System.out.print(i+" ");
+            if (j == m)
+                System.out.print(i + " ");
         }
+        System.out.println();
     }
-    static void patternSearchingNaiveImprove(String text,String pattern){
-        int m=pattern.length();
-        int n=text.length();
-        for (int i=0;i<=(n-m); ){
+
+    static void searchPattern2(String text, String pattern) {
+        int n = text.length();
+        int m = pattern.length();
+
+        for (int i = 0; i <= n - m; i++) {
             int j;
-            for (j=0;j<m-1;j++)
-                if (pattern.charAt(j)!=text.charAt(i+j))
+
+            for (j = 0; j < m; j++) {
+                if (text.charAt(i + j) != pattern.charAt(j)) {
                     break;
-            if (j==m)
-                System.out.print(i+" ");
-            if (j==0)
-                i++;
-            else
-                i=i+j;
+                }
+            }
+
+            if (j == m) {
+                System.out.print(i + " ");
+            }
         }
     }
 }
