afdb9a0a5896b03be553d4667e87b457fbf1678a
manindrak <manindratoyou@gmail.com>
2025-05-09 16:05:28 +0530

    LRUCache

---
+++ src/com/manindra/linkedlist_leetcode/LRUCache.java
@@ -0,0 +1,78 @@
+package com.manindra.linkedlist_leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache { //solution 146
+
+    private final int capacity;
+    private final Map<Integer, Node> map;
+    private final Node head, tail;
+
+    private static class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int k, int v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node(0, 0); // dummy head
+        tail = new Node(0, 0); // dummy tail
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key))
+            return -1;
+        Node node = map.get(key);
+        remove(node);
+        insertToFront(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            remove(map.get(key));
+        } else if (map.size() == capacity) {
+            Node lru = tail.prev;
+            remove(lru);
+            map.remove(lru.key);
+        }
+        Node newNode = new Node(key, value);
+        insertToFront(newNode);
+        map.put(key, newNode);
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insertToFront(Node node) {
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+        node.prev = head;
+    }
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2); // capacity = 2
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1)); // returns 1
+        cache.put(3, 3);    // evicts key 2
+        System.out.println(cache.get(2)); // returns -1 (not found)
+        cache.put(4, 4);    // evicts key 1
+        System.out.println(cache.get(1)); // returns -1
+        System.out.println(cache.get(3)); // returns 3
+        System.out.println(cache.get(4)); // returns 4
+    }
+
+}
