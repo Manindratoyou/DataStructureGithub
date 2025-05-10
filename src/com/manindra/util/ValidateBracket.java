2d7c859ff514b81e3b27b375b5288bceb9e2fc73
manindrak <manindratoyou@gmail.com>
2025-05-10 20:41:20 +0530

    updated code

--- src/com/manindra/util/ValidateBracket.java
+++ src/com/manindra/util/ValidateBracket.java
@@ -5,24 +5,24 @@ import java.util.Stack;
 public class ValidateBracket {
 
     public static void main(String[] args) {
-        String bracket= "{()[{()()}]}";
+        String bracket = "{()[{()()}]}";
         System.out.println(validateBracket(bracket));
     }
 
-    static boolean validateBracket(String bracket){
+    static boolean validateBracket(String bracket) {
 
-        Stack<Character> characterStack=new Stack<>();
-        for (int i=0;i<bracket.length();i++){
-            char currentCharactor=bracket.charAt(i);
-            if (currentCharactor=='(' || currentCharactor=='{' || currentCharactor=='['){
-                characterStack.push(currentCharactor);
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < bracket.length(); i++) {
+            char currentCharactor = bracket.charAt(i);
+            if (currentCharactor == '(' || currentCharactor == '{' || currentCharactor == '[') {
+                stack.push(currentCharactor);
             }
-            if ((currentCharactor==')' && characterStack.peek()=='(') ||
-                    (currentCharactor=='}' && characterStack.peek()=='{') ||
-                    (currentCharactor==']' && characterStack.peek()=='[')){
-                characterStack.pop();
+            if ((currentCharactor == ')' && stack.peek() == '(') ||
+                    (currentCharactor == '}' && stack.peek() == '{') ||
+                    (currentCharactor == ']' && stack.peek() == '[')) {
+                stack.pop();
             }
         }
-        return characterStack.isEmpty();
+        return stack.isEmpty();
     }
 }
