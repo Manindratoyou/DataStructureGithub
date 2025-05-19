90b7779a41dd5f96a34e0310840b1e9943b056e9
manindrak <manindratoyou@gmail.com>
2025-05-19 21:32:08 +0530

    updated code

--- src/com/manindra/heap_ds_leetcode/ReorganizeString.java
+++ src/com/manindra/heap_ds_leetcode/ReorganizeString.java
@@ -20,14 +20,14 @@ public class ReorganizeString { //solution 767
     }
 
     String reorganizeString(String str) {
-        int[] hash = new int[26];
+        int[] arr = new int[26];
         for (int i = 0; i < str.length(); i++)
-            hash[str.charAt(i) - 'a']++;
+            arr[str.charAt(i) - 'a']++;
 
         int max = 0, letter = 0;
-        for (int i = 0; i < hash.length; i++)
-            if (hash[i] > max) {
-                max = hash[i];
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] > max) {
+                max = arr[i];
                 letter = i;
             }
 
@@ -38,14 +38,14 @@ public class ReorganizeString { //solution 767
 
         // Fill all even places with majority character
         int idx = 0;
-        while (hash[letter]-- > 0) {
+        while (arr[letter]-- > 0) {
             res[idx] = (char) (letter + 'a');
             idx += 2;
         }
 
         // Fill the remaining characters
-        for (int i = 0; i < hash.length; i++)
-            while (hash[i]-- > 0) {
+        for (int i = 0; i < arr.length; i++)
+            while (arr[i]-- > 0) {
                 if (idx >= res.length)
                     idx = 1;
 
