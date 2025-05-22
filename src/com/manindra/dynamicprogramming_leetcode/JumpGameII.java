763d40251fb573d76af1ab1f55d33474cc97f569
manindrak <manindratoyou@gmail.com>
2025-05-22 14:18:07 +0530

    MinimumJumps

--- src/com/manindra/dynamicprogramming_leetcode/JumpGameII.java
+++ src/com/manindra/dynamicprogramming_leetcode/JumpGameII.java
@@ -15,6 +15,13 @@ public class JumpGameII { //solution 45
         System.out.println(jump(nums3)); // Expected output: 2
         System.out.println(jump(nums4)); // Expected output: 3
         System.out.println(jump(nums5)); // Expected output: 4
+
+        System.out.println("================");
+        System.out.println(minJumps(nums1,nums1.length)); // Expected output: 2
+        System.out.println(minJumps(nums2,nums2.length)); // Expected output: 2
+        System.out.println(minJumps(nums3,nums3.length)); // Expected output: 2
+        System.out.println(minJumps(nums4,nums4.length)); // Expected output: 3
+        System.out.println(minJumps(nums5,nums5.length)); // Expected output: 4
     }
 
     static int jump(int[] nums) {
@@ -46,6 +53,27 @@ public class JumpGameII { //solution 45
         }
 
         return totalJumps;
+    }
+
+    static int minJumps(int arr[], int n)
+    {
+
+        int dp[]= new int[n];
+        int i, j;
+
+        dp[0] = 0;
+
+
+        for (i = 1; i < n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (j = 0; j < i; j++) {
+                if (i <= j + arr[j] && dp[j] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                    break;
+                }
+            }
+        }
+        return dp[n - 1];
     }
 
 }
