375a3a001485d3877165a8862ee0a7631eb0de0c
manindrak <manindratoyou@gmail.com>
2025-05-22 18:40:25 +0530

    updated code

--- src/com/manindra/leetcodeEasy/CheckIfTheSentenceIsPangram.java
+++ src/com/manindra/leetcodeEasy/CheckIfTheSentenceIsPangram.java
@@ -43,8 +43,32 @@ public class CheckIfTheSentenceIsPangram { //solution 1832
     }
 
     public boolean checkIfPangram3(String sentence) {
-        for (char i = 97; i< 123; i++){
-            if (sentence.indexOf(i) == -1) return false;
+        for (char i = 97; i < 123; i++) {
+            if (sentence.indexOf(i) == -1)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean checkIfPangram4(String sentence) {
+
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < sentence.length(); i++) {
+            set.add(sentence.charAt(i));
+        }
+        return set.size() == 26;
+    }
+
+    public boolean checkIfPangram5(String sentence) {
+
+        boolean[] arr = new boolean[26];
+
+        for (int i = 0; i < sentence.length(); i++) {
+            arr[sentence.charAt(i) - 'a'] = true;
+        }
+        for (boolean curr : arr) {
+            if (!curr)
+                return false;
         }
         return true;
     }
