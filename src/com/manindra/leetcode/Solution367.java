54e9f5ee67993a66bb220d2021d4e9e18595c70e
manindra <manindratoyou@gmail.com>
2025-05-23 23:59:25 +0530

    updated code

--- src/com/manindra/leetcode/Solution367.java
+++ src/com/manindra/leetcode/Solution367.java
@@ -4,37 +4,56 @@ public class Solution367 { //valid perfect square
 
     public static void main(String[] args) {
 
-        int num1=16;
-        int num2=14;
+        int num1 = 16;
+        int num2 = 14;
         //System.out.println(num1 + " is a valid perfect square : "+isPerfectSquare(num1));
         //System.out.println(num2 + " is a valid perfect square : "+isPerfectSquare(num2));
-        System.out.println(num1 + " is a valid perfect square : "+isPerfectSquareSolutionTwo(num1));
-        System.out.println(num2 + " is a valid perfect square : "+isPerfectSquareSolutionTwo(num2));
+        System.out.println(num1 + " is a valid perfect square : " + isPerfectSquareSolutionTwo(num1));
+        System.out.println(num2 + " is a valid perfect square : " + isPerfectSquareSolutionTwo(num2));
     }
 
-    public static boolean isPerfectSquare(int num){
-        if (num<2) return true;//0 and 1 are perfect square
+    public static boolean isPerfectSquare(int num) {
+        if (num < 2) return true;//0 and 1 are perfect square
 
-        long left =2;
-        long right=num/2;
+        long left = 2;
+        long right = num / 2;
 
-        while (left<=right){
-            long mid=left+(right-left)/2;
-            long square=mid*mid;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
 
-            if (square==num) return true;
-            else if (square<num) left=mid+1;
-            else right=mid-1;
+            if (square == num)
+                return true;
+            else if (square < num)
+                left = mid + 1;
+            else right = mid - 1;
         }
         return false;
     }
-    public static boolean isPerfectSquareSolutionTwo(int num){
-        int i=1;
-        while (num>0){
-            num-=i;
-            i+=2;
+
+    public static boolean isPerfectSquareSolutionTwo(int num) {
+        int i = 1;
+        while (num > 0) {
+            num -= i;
+            i += 2;
         }
-        return num==0;
+        return num == 0;
+    }
+
+    boolean isPerfectSquareMaths(int num) {
+        int oddNumber = 1;
+        int sum = 1;
+        while (sum <= num) {
+            // Found the perfect square
+            if (sum == num) {
+                return true;
+            }
+
+            oddNumber += 2; // Next odd number
+            sum += oddNumber;
+        }
+
+        return false;
     }
 
 }
