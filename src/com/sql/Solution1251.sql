f8491d6e79529e11d165583d4ee95ec91c796c3e
manindra <manindratoyou@gmail.com>
2025-06-18 21:10:12 +0530

    SQL Code

---
+++ src/com/sql/Solution1251.sql
@@ -0,0 +1,18 @@
+--Mysql
+SELECT p.product_id,
+       IFNULL(ROUND(SUM(p.price * u.units) / SUM(u.units), 2), 0) AS average_price
+FROM prices p
+LEFT JOIN unitssold u
+  ON p.product_id = u.product_id
+ AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
+
+--Oracle
+SELECT p.product_id,
+       NVL(ROUND(SUM(p.price * u.units) / SUM(u.units), 2), 0) AS average_price
+FROM prices p
+LEFT JOIN unitssold u
+  ON p.product_id = u.product_id
+ AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id
+
