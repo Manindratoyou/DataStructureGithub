59be087ef16b4b0160ea32db1f27bb92f21c1792
manindra <manindratoyou@gmail.com>
2025-06-24 23:26:41 +0530

    updated code

--- src/com/manindra/leetcode/Solution136.java
+++ src/com/manindra/leetcode/Solution136.java
@@ -3,25 +3,29 @@ package com.manindra.leetcode;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Solution136 {
+public class Solution136 { //Single Number
 
     public static void main(String[] args) {
 
-        int [] arr={2,2,1};
+        int[] arr = {2, 2, 1};
+        int[] arr1 = {4, 1, 2, 1, 4};
+        int[] arr2 = {3, 4, 9, 3, 7, 4, 8, 9, 8};
         System.out.println(singleNumber(arr));
+        System.out.println(singleNumber(arr1));
         System.out.println(singleNumberSolutionTwo(arr));
+        System.out.println(singleNumberSolutionThree(arr2));
     }
 
-    static int singleNumber(int [] arr){
-
-        int result=0;
+    static int singleNumber(int[] arr) {
+        int result = 0;
         for (int num : arr)
-            result^=num;
+            result ^= num;
         return result;
     }
-    static int singleNumberSolutionTwo(int [] arr){
-        Set<Integer> set=new HashSet<>();
-        for (int num : arr){
+
+    static int singleNumberSolutionTwo(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : arr) {
             if (set.contains(num))
                 set.remove(num);
             else
@@ -29,7 +33,8 @@ public class Solution136 {
         }
         return set.iterator().next();
     }
-    public int singleNumberSolutionThree(int[] nums) {
+
+    static int singleNumberSolutionThree(int[] nums) {
         int sing = nums[0];
         for (int i = 1; i < nums.length; i++) {
             sing = sing ^ nums[i];
