bb291e6a3be5924b9498209a219913ba8af702f7
manindra <manindratoyou@gmail.com>
2025-07-02 00:45:40 +0530

    RotateArray

---
+++ src/com/manindra/leetcodemedium_2/RotateArray.java
@@ -0,0 +1,50 @@
+package com.manindra.leetcodemedium_2;
+
+public class RotateArray { //solution 189
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
+
+        rotate(nums, k);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; // In case k > n
+
+        reverse(nums, 0, n - 1);         // Step 1
+        reverse(nums, 0, k - 1);         // Step 2
+        reverse(nums, k, n - 1);         // Step 3
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
+
+    public static void rotate2(int[] nums, int k) {
+        int n = nums.length;
+        int[] temp = new int[n];
+        k = k % n;
+
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+
+        // Copy back to original array
+        for (int i = 0; i < n; i++) {
+            nums[i] = temp[i];
+        }
+    }
+
+
+}
+
