11cfc9aaa56239436239ae701d4d34cab1181d27
manindra <manindratoyou@gmail.com>
2025-07-07 01:44:11 +0530

    RemoveDuplicatesII

--- src/com/manindra/matrix/ExampleOne.java
+++ src/com/manindra/matrix/ExampleOne.java
@@ -3,11 +3,31 @@ package com.manindra.matrix;
 public class ExampleOne {
 
     public static void main(String[] args) {
-        int [] [] arr={{1,2,3,8,9},
-                       {4,5,6}};
+        int[][] arr = {{1, 2, 3, 8, 9}, {4, 5, 6}};
 
-        for (int i=0;i<arr.length;i++)
-            for (int j=0;j<arr[i].length;j++)
-                System.out.print(arr[i][j]+ " ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("==================");
+        printMatrixIndex2();
     }
+
+    public static void printMatrixIndex2() {
+        int rows = 4;
+        int cols = 4;
+
+        for (int i = 0; i < rows; i++) {
+            // Inner loop for columns
+            // The loop will run from 0 to cols-1 (i.e., 0, 1, 2, 3 for 4 columns)
+            for (int j = 0; j < cols; j++) {
+                System.out.print(i + "," + j + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+
 }
--- src/com/manindra/matrix/ExampleThree.java
+++ src/com/manindra/matrix/ExampleThree.java
@@ -2,7 +2,7 @@ package com.manindra.matrix;
 
 public class ExampleThree {
 
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
 
         int m=3;
         int [] [] arr=new int[m][];
@@ -11,6 +11,17 @@ public class ExampleThree {
             for (int j=0;j< arr[i].length;j++){
                 arr[i][j]=i;
                 System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int rows = 3;  // You can change this to generate more rows
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
