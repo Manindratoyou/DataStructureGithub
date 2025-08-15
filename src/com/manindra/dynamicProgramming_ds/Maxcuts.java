56fafe25ad47964e886773cf673e1a25518fb502
manindra <manindratoyou@gmail.com>
2025-08-15 20:02:08 +0530

    updated code

--- src/com/manindra/dynamicProgramming_ds/Maxcuts.java
+++ src/com/manindra/dynamicProgramming_ds/Maxcuts.java
@@ -2,33 +2,45 @@ package com.manindra.dynamicProgramming_ds;
 
 public class Maxcuts {
 
-    public static void main (String[] args)
-    {
+    public static void main(String[] args) {
         int n = 5, a = 1, b = 2, c = 3;
 
+        System.out.println(maxCutsRecursion(n, a, b, c));
         System.out.println(maxCuts(n, a, b, c));
 
 
     }
 
-    static int maxCuts(int n, int a, int b, int c)
-    {
+    static int maxCutsRecursion(int n, int a, int b, int c) {
 
-        int dp[]= new int[n+1];
+        if (n < 0)
+            return -1;
+        if (n == 0)
+            return 0;
+        int result = Math.max(maxCutsRecursion(n - a, a, b, c),
+                Math.max(maxCutsRecursion(n - b, a, b, c), maxCutsRecursion(n - c, a, b, c)));
 
-        dp[0] =0;
+        if (result == -1)
+            return result;
+        return result + 1;
+    }
 
-        for(int i = 1; i<=n; i++)
-        {
+    static int maxCuts(int n, int a, int b, int c) {
+
+        int dp[] = new int[n + 1];
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
             dp[i] = -1;
 
-            if(i-a >=0) dp[i] = Math.max(dp[i],dp[i-a]);
+            if (i - a >= 0) dp[i] = Math.max(dp[i], dp[i - a]);
 
-            if(i-b >=0) dp[i] = Math.max(dp[i],dp[i-b]);
+            if (i - b >= 0) dp[i] = Math.max(dp[i], dp[i - b]);
 
-            if(i-c >=0) dp[i] = Math.max(dp[i],dp[i-c]);
+            if (i - c >= 0) dp[i] = Math.max(dp[i], dp[i - c]);
 
-            if(dp[i]!=-1)
+            if (dp[i] != -1)
                 dp[i]++;
         }
 
--- src/com/manindra/dynamicProgramming_ds/MinCoins.java
+++ src/com/manindra/dynamicProgramming_ds/MinCoins.java
@@ -2,17 +2,35 @@ package com.manindra.dynamicProgramming_ds;
 
 public class MinCoins {
 
-    public static void main (String[] args)
-    {
-        int arr[] = {3, 4, 1}, val =5, n =3;
+    public static void main(String[] args) {
+        int arr[] = {3, 4, 1}, val = 5;
+        int arr1[] = {25, 10, 5}, val1 = 30; //op ->  2
 
 
-        System.out.println(minCoins(arr, n, val));
+        System.out.println(minCoins(arr, arr.length, val));
+        System.out.println(minCoins(arr1, arr.length, val1));
 
 
     }
-    static int minCoins(int arr[], int m, int value)
-    {
+
+    static int minCoinsRecursion(int arr[], int n, int value) {
+
+        if (value == 0)
+            return 0;
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= value) {
+                int sub_res = minCoinsRecursion(arr, n, value - arr[i]);
+                if (sub_res != Integer.MAX_VALUE) {
+                    result = Math.min(result, sub_res + 1);
+                }
+            }
+        }
+        return result;
+
+    }
+
+    static int minCoins(int arr[], int n, int value) {
 
         int dp[] = new int[value + 1];
 
@@ -24,12 +42,10 @@ public class MinCoins {
             dp[i] = Integer.MAX_VALUE;
 
 
-        for (int i = 1; i <= value; i++)
-        {
+        for (int i = 1; i <= value; i++) {
 
-            for (int j = 0; j < m; j++)
-                if (arr[j] <= i)
-                {
+            for (int j = 0; j < n; j++)
+                if (arr[j] <= i) {
                     int sub_res = dp[i - arr[j]];
                     if (sub_res != Integer.MAX_VALUE
                             && sub_res + 1 < dp[i])
